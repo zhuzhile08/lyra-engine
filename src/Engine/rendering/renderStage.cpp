637ed9b97a373653adc928cf755834d6d359c83a
Zhile <zhuzhile08@gmail.com>
2022-03-20 12:04:58 +0100

    add a "sub"renderer

---
+++ src/Engine/rendering/renderStage.cpp
@@ -0,0 +1,72 @@
+#include <rendering/renderStage.h>
+
+namespace lyra {
+
+RenderStage::RenderStage() { ("RenderStage", nullptr); }
+
+void RenderStage::destroy() {
+    var.framebuffers.destroy();
+    var.pipeline.destroy();
+    var.descriptors.destroy();
+    for (auto& commandBuffer : var.commandBuffers) commandBuffer.destroy();
+}
+
+void RenderStage::create(Renderer renderer, uint16 descriptorCount, VulkanDescriptor::Writer writer) {
+    set_parent(&renderer);
+
+    var.framebuffers.create_render_pass(renderer.var.device, renderer.var.swapchain);
+    var.framebuffers.create();
+    for (auto& cmdBuff : var.commandBuffers) cmdBuff.create(renderer.var.device, renderer.var.commandPool);
+    var.descriptors.create(renderer.var.device, descriptorCount, renderer.var.descriptorSetLayout, renderer.var.descriptorPool, writer);
+}
+
+void RenderStage::record_command_buffers(int index, int currentFrame) {
+    // look at how D Y N A M I C this is
+    var.commandBuffers[currentFrame].begin(0);
+
+    begin_render_pass(currentFrame, var.framebuffers.begin_info(index));
+
+    bind_pipeline(var.pipeline, currentFrame);
+
+    var.bind_queue.flush();
+
+    bind_descriptor(var.descriptors, var.pipeline, currentFrame);
+
+    var.draw_queue.flush();
+
+    end_render_pass(currentFrame);
+
+    var.commandBuffers[currentFrame].end();
+}
+
+void RenderStage::draw(Renderer renderer, int currentFrame) const {
+    renderer.var.render_queue.add([&]() { var.commandBuffers[currentFrame].reset(); });
+    renderer.var.render_queue.add([&]() { var.commandBuffers[currentFrame].record(); });
+}
+
+void RenderStage::bind_descriptor(const VulkanDescriptor descriptor, const VulkanGraphicsPipeline pipeline, int cmdBuffIndex) const {
+    vkCmdBindDescriptorSets(var.commandBuffers[cmdBuffIndex].get(), VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.get().pipelineLayout, 0, 1, descriptor.get().data(), 0, nullptr);
+}
+
+void RenderStage::begin_render_pass(int cmdBuffIndex, const VkRenderPassBeginInfo beginInfo) const {
+    vkCmdBeginRenderPass(var.commandBuffers[cmdBuffIndex].get(), &beginInfo, VK_SUBPASS_CONTENTS_INLINE);
+}
+
+void RenderStage::end_render_pass(int cmdBuffIndex) const {
+    vkCmdEndRenderPass(var.commandBuffers[cmdBuffIndex].get());
+}
+
+void RenderStage::bind_pipeline(VulkanGraphicsPipeline pipeline, int cmdBuffIndex) const {
+    vkCmdBindPipeline(var.commandBuffers[cmdBuffIndex].get(), VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.get().graphicsPipeline);
+}
+
+void RenderStage::bind_model(const VkBuffer vertexBuffer, const VkBuffer indexBuffer, int cmdBuffIndex) const {
+    vkCmdBindVertexBuffers(var.commandBuffers[cmdBuffIndex].get(), 0, 1, &vertexBuffer, 0);
+    vkCmdBindIndexBuffer(var.commandBuffers[cmdBuffIndex].get(), indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+}
+
+void RenderStage::draw_model(const uint32 size, int cmdBuffIndex) const {
+    vkCmdDrawIndexed(var.commandBuffers[cmdBuffIndex].get(), size, 1, 0, 0, 0);
+}
+
+} // namespace lyra
---
+++ src/Engine/rendering/renderStage.h
@@ -0,0 +1,131 @@
+/*************************
+ * @file renderer.h
+ * @author Zhile Zhu (zhuzhile08@gmail.com)
+ *
+ * @brief a rendering system around the Vulkan API with basic features
+ *
+ * @date 2022-02-04
+ *
+ * @copyright Copyright (c) 2022 Zhile Zhu
+ *************************/
+
+#pragma once
+
+#include <core/defines.h>
+#include <core/queue_types.h>
+#include <rendering/vulkan/pipeline.h>
+#include <rendering/vulkan/command_buffer.h>
+#include <rendering/vulkan/descriptor.h>
+#include <rendering/vulkan/framebuffer.h>
+#include <rendering/vulkan/swapchain.h>
+#include <rendering/renderer.h>
+
+#include <core/logger.h>
+
+#include <memory>
+#include <vector>
+
+#include <noud.h>
+#include <vulkan/vulkan.h>
+
+namespace lyra {
+
+/**
+ * @brief a vulkan renderer with basic features
+ * @todo maybe abstract vma memory allocation?
+ */
+class RenderStage : noud::Node {
+private:
+    /**
+     * @brief struct containing all the variabels
+     */
+    struct          Variables {
+        VulkanFramebuffers                  framebuffers;
+        VulkanDescriptor                    descriptors;
+        std::vector<VulkanCommandBuffer>    commandBuffers;
+        VulkanGraphicsPipeline              pipeline;
+
+        CallQueue                           bind_queue;
+        CallQueue                           draw_queue;
+
+        bool                                drawing = true;
+    };
+
+public:
+    RenderStage();
+    /**
+     * @brief destroy the renderer
+     */
+    void            destroy();
+    /**
+     * @brief create the renderer
+     *
+     * @param renderer the renderer
+     */
+    void            create(Renderer renderer, uint16 descriptorCount, VulkanDescriptor::Writer writer);
+    
+    /**
+     * @brief bind the functions for resetting and finish recording the command buffers
+     * 
+     * @param renderer the renderer. Just set it to the parent
+     * @param currentFrame the current draw frame
+     */
+    void            draw(Renderer renderer, int currentFrame) const;
+
+private:
+    Variables       var;
+
+    /**
+     * @brief bind a descriptor set
+     *
+     * @param descriptor descriptor
+     * @param pipelineLayout pipeline layout
+     */
+    void            bind_descriptor(const VulkanDescriptor descriptor, const VulkanGraphicsPipeline pipeline, int cmdBuffIndex) const;
+    
+    void            push_constants() const;
+    /**
+     * @brief begin render passes
+     *
+     * @param framebuffers frame buffer
+     * @param index index of the frame buffer
+     */
+    void            begin_render_pass(int cmdBuffIndex, const VkRenderPassBeginInfo beginInfo) const;
+    /**
+     * @brief end render passes
+     *
+     * @param framebuffers frame buffer
+     */
+    void            end_render_pass(int cmdBuffIndex) const;
+    /**
+     * @brief bind the graphics pipeline
+     *
+     * @param pipeline pipeline
+     */
+    void            bind_pipeline(const VulkanGraphicsPipeline pipeline, int cmdBuffIndex) const;
+    /**
+     * @brief bind a model
+     *
+     * @param vertexBuffer vertex buffer of the model
+     * @param indexBuffer index buffer of the model
+     * @param cmdBuffIndex index of the command buffer to bind the model
+     */
+    void            bind_model(const VkBuffer vertexBuffer, const VkBuffer indexBuffer, int cmdBuffIndex) const;
+    /**
+     * @brief draw a model
+     *
+     * @param size size of the index buffer
+     * @param cmdBuffIndex index of the command buffer to draw the model
+     */
+    void            draw_model(const uint32 size, int cmdBuffIndex) const;
+
+    /**
+     * @brief record all the commands
+     */
+    void            record_command_buffers(int index, int currentFrame);
+
+    friend class    Mesh;
+    friend class    Texture;
+};
+
+}
