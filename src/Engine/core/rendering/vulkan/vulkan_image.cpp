c93b1f35c882e7cc2201fee298ae726225a5c60a
Zhile <zhuzhile08@gmail.com>
2022-04-15 12:26:59 +0200

    small bugfix

--- src/Engine/core/rendering/vulkan/vulkan_image.cpp
+++ src/Engine/core/rendering/vulkan/vulkan_image.cpp
@@ -102,7 +102,7 @@ const {
 		sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT; destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
 	} else LOG_EXEPTION("Invalid image layout transition was requested whilst transitioning an image layout at: ", GET_ADDRESS(this));
 
-	vkCmdPipelineBarrier(cmdBuff.get(), sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, get_barrier(sourceAccess, destinationAccess, oldLayout, newLayout, subresourceRange));
+	vkCmdPipelineBarrier(cmdBuff.get(), sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, get_image_memory_barrier(sourceAccess, destinationAccess, oldLayout, newLayout, subresourceRange));
 
 	// end recording
 	cmdBuff.end();
@@ -115,7 +115,7 @@ const {
 	cmdBuff.destroy();
 }
 
-const VkImageMemoryBarrier* VulkanImage::get_barrier(
+const VkImageMemoryBarrier* VulkanImage::get_image_memory_barrier(
 	const VkAccessFlags srcAccessMask,
 	const VkAccessFlags dstAccessMask,
 	const VkImageLayout srcLayout,
