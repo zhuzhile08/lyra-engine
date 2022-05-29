8662073c24556a1642578185f6ec276e4601a9bc
Zhile <zhuzhile08@gmail.com>
2022-05-29 14:28:52 +0200

    added extra output location for shaders

--- compileShader.bat
+++ compileShader.bat
@@ -1,3 +1,6 @@
 C:/VulkanSDK/1.2.198.1/Bin/glslc.exe src/shaders/src/vert.vert -o data/shader/vert.spv
 C:/VulkanSDK/1.2.198.1/Bin/glslc.exe src/shaders/src/frag.frag -o data/shader/frag.spv
+
+C:/VulkanSDK/1.2.198.1/Bin/glslc.exe src/shaders/src/vert.vert -o out/build/x64-Debug/data/shader/vert.spv
+C:/VulkanSDK/1.2.198.1/Bin/glslc.exe src/shaders/src/frag.frag -o out/build/x64-Debug/data/shader/frag.spv
 pause
