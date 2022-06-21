108282bc11b633a6c82adabfa80e167a1e0a2e3d
Zhile <zhuzhile08@gmail.com>
2022-06-21 17:23:21 +0200

    add content manager

---
+++ ContentManager/cmake/lyra-config.cmake
@@ -0,0 +1,10 @@
+set(LYRA_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/LyraEngine/src/")
+
+# Support both 32 and 64 bit builds
+if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
+  set(LYRA_LIBRARIES "${CMAKE_SOURCE_DIR}/out/build/x64-debug/LyraEngine/LyraEngine.lib") # don't forget to change these to the release builds
+else ()
+  set(LYRA_LIBRARIES "${CMAKE_SOURCE_DIR}/out/build/x64-debug/LyraEngine/LyraEngine.lib") # also don't forget to change this to the x86 version
+endif ()
+
+string(STRIP "${LYRA_LIBRARIES}" LYRA_LIBRARIES)
