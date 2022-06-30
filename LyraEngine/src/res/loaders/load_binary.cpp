7e52a39809fce22db85dabb8663ca43303c2f0c8
Zhile <zhuzhile08@gmail.com>
2022-06-30 17:18:34 +0200

    add json loading

--- LyraEngine/src/res/loaders/load_binary.cpp
+++ LyraEngine/src/res/loaders/load_binary.cpp
@@ -7,9 +7,6 @@ const non_access::AssetFile& load_binary(std::string binPath) {
 
 	// check
 	if (binPath.substr(binPath.length() - 4, 4) != "ldat") Logger::log_warning("Non standard file extension found on data file at path: ", binPath, "! This may cause problems during loading.");
-	// create the path for the compressed json
-	std::string jsonPath = binPath;
-	jsonPath.replace(binPath.length() - 4, 4, "lson");
 
 	// load the binary
 	std::ifstream binInFile;
@@ -20,12 +17,19 @@ const non_access::AssetFile& load_binary(std::string binPath) {
 	// read the file contentents
 	binInFile.seekg(0);
 
+	// read the type of the asset
 	binInFile.read(loadedAsset.type, 4);
 
+	// length of the binary file
 	uint32_t binLength = 0;
-	binInFile.read((char*)&binLength, sizeof(uint32_t));
+	binInFile.read((char*)&binLength, sizeof(uint32));
+	// resize and read the binary file
 	loadedAsset.binary.resize(binLength);
 	binInFile.read(loadedAsset.binary.data(), binLength);
+
+	// create the path for the compressed json
+	std::string jsonPath = binPath;
+	jsonPath.replace(binPath.length() - 4, 4, "lson");
 
 	// load the json
 	std::ifstream jsonInFile;
@@ -33,12 +37,36 @@ const non_access::AssetFile& load_binary(std::string binPath) {
 	if (!jsonInFile.is_open()) Logger::log_exception("Failed to open an asset file for reading at path: ", binPath, "!");
 	Logger::log_debug(Logger::tab(), "Successfully loaded asset file in at path ", binPath, "!");
 
+	// read the file contentents
+	binInFile.seekg(0);
+
+	// read the type of the json
+	char type[4] = { };
+	jsonInFile.read(type, sizeof(char[4]));
+	if (type != loadedAsset.type) Logger::log_exception("Loaded compressed JSON file at: ", jsonPath, " is not the same type as the corresponding asset file loaded at: ", binPath, "!"); // check if binary and json are for the same type of stuff
+
+	// length of the compressed json
 	uint32_t jsonLength = 0;
-	jsonInFile.read((char*)&jsonLength, sizeof(uint32_t));
-	loadedAsset.json.resize(jsonLength);
-	jsonInFile.read(loadedAsset.json.data(), jsonLength);
+	jsonInFile.read((char*)&jsonLength, sizeof(uint32));
+
+	// length of the decompressed json
+	uint32_t jsonDecompLength = 0;
+	jsonInFile.read((char*)&jsonDecompLength, sizeof(uint32));
+
+	// read the compressed json
+	char* json;
+	jsonInFile.read(json, jsonLength);
+	// decompress the json
+	json = unpack_json(json, jsonLength, jsonDecompLength);
+	loadedAsset.json = nlohmann::json::parse(json);
 
 	return loadedAsset;
+}
+
+char*& unpack_json(const char* const data, uint32 jsonLength, uint32 jsonSize) {
+	char* result;
+	LZ4_decompress_safe(data, result, jsonLength, jsonSize);
+	return result;
 }
 
 } // namespace lyra
--- LyraEngine/src/res/loaders/load_binary.h
+++ LyraEngine/src/res/loaders/load_binary.h
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <json.hpp>
+#include <lz4.h>
 
 namespace lyra {
 
@@ -12,7 +14,7 @@ namespace non_access {
 
 struct AssetFile {
 	char type[4];
-	std::string json;
+	nlohmann::json json;
 	std::vector<char> binary;
 };
 
@@ -22,16 +24,37 @@ struct AssetFile {
  * @brief load a asset file
  *
  * @param binPath path for the binary file
+ * 
  * @return const lyra::non_access::AssetFile
  */
 const non_access::AssetFile& load_binary(std::string binPath);
 
 /**
+ * @brief ldat file format documentation:
+ * 
+ * 4 bytes, char, type of binary
+ * 4 bytes, uint32_t, length of the compressed data
+ * the rest is the compressed data
+ */
+
+/**
  * @brief decompress the json of a asset file
  * 
  * @param assetFile asset file
- * @return const lyra::non_access:AssetFile&
+ * @param jsonLength length of the compressed json file
+ * @param jsonSize size of the original json file
+ * 
+ * @return char*&
  */
-const non_access::AssetFile& unpack_json(non_access::AssetFile& assetFile);
+char* &unpack_json(const char* const data, uint32 jsonLength, uint32 jsonSize);
+
+/**
+ * @brief lson file format documentation:
+ * 
+ * 4 bytes, char, type of json
+ * 4 bytes, uint32_t, length of the compressed json data
+ * 4 bytes, uint32_t, length of the original json file
+ * the rest is the compressed json
+ */
 
 } // namespace lyra
