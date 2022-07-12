e831eda08c067b941f44fcd2413db0f9365e3e84
Zhile <zhuzhile08@gmail.com>
2022-07-12 21:56:59 +0200

    changed vulkan path

--- compileShader.bat
+++ compileShader.bat
@@ -1,6 +1,6 @@
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
 
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe LyraEngine/src/shaders/src/vert.vert -o out/build/x64-debug/LyraGui/data/shader/vert.spv
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe LyraEngine/src/shaders/src/frag.frag -o out/build/x64-debug/LyraGui/data/shader/frag.spv
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe LyraEngine/src/shaders/src/vert.vert -o out/build/x64-debug/LyraGui/data/shader/vert.spv
+C:/VulkanSDK/1.3.204.1/Bin/glslc.exe LyraEngine/src/shaders/src/frag.frag -o out/build/x64-debug/LyraGui/data/shader/frag.spv
 pause
