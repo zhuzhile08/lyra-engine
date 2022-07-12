907a898ade933a92e985722d306e29f093e85b1f
Zhile <zhuzhile08@gmail.com>
2022-07-12 21:56:51 +0200

    fix some cmake issues

--- cmake/lyra-config.cmake
+++ cmake/lyra-config.cmake
@@ -4,7 +4,7 @@ set(LYRA_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/LyraEngine/src")
 if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
   set(LYRA_LIBRARIES "${CMAKE_SOURCE_DIR}/out/build/x64-debug/LyraEngine/LyraEngine.lib") # don't forget to change these to the release builds
 else ()
-  set(LYRA_LIBRARIES "${CMAKE_SOURCE_DIR}/out/build/x64-debug/LyraEngine/LyraEngine.lib") # also don't forget to change this to the x86 version
+  set(LYRA_LIBRARIES "${CMAKE_SOURCE_DIR}/out/build/x86-debug/LyraEngine/LyraEngine.lib") # also don't forget to change this to the x86 version
 endif ()
 
 string(STRIP "${LYRA_LIBRARIES}" LYRA_LIBRARIES)
