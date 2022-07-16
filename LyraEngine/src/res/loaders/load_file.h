0967e02fbfda544e32c8878b91314f75a73ba07a
Zhile <zhuzhile08@gmail.com>
2022-07-16 17:56:32 +0200

    update file loaders to be more generalized

--- LyraEngine/src/res/loaders/load_file.h
+++ LyraEngine/src/res/loaders/load_file.h
@@ -17,11 +17,33 @@ enum OpenMode {
 
 /**
  * @brief load a file
- * 
+ *
  * @param path path
  * @param mode mode to open the file
- * @param file file to load the file into
+ * @param file the file
  */
 void load_file(const char* path, const int mode, std::ifstream& file);
+
+/**
+ * @brief load a file and store all of its contents
+ *
+ * @tparam data type to store the file
+ * @param path path
+ * @param mode mode to open the file
+ * @param data type to dump the file into
+ */
+template<class _Ty> void load_file(const char* path, const int mode, _Ty* data) {
+	// load the binary
+	std::ifstream file;
+	load_file(path, mode, file);
+
+	file.seekg(0, file.end);
+	size_t size = (size_t)file.tellg();
+	file.seekg(0, file.beg);
+
+	data->resize(size);
+	file.read(data->data(), size);
+	file.close();
+}
 
 } // namespace lyra
