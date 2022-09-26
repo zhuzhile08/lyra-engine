fe61de3d537b8eeabce4df1ae6de0b8d1cec35e5
zhuzhile08 <zhuzhile08@gmail.com>
2022-09-26 22:01:02 +0200

    Add more OSX specific code and optimizations

---
+++ compileShader.sh
@@ -0,0 +1,5 @@
+/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
+/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
+
+/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/x64-debug/LyraGui/data/shader/vert.spv
+/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/x64-debug/LyraGui/data/shader/frag.spv
