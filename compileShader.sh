f1f95c4282e1ec065b82ddd4dd55703884490852
zhuzhile08 <zhuzhile08@gmail.com>
2022-09-28 18:20:23 +0200

    fixes

--- compileShader.sh
+++ compileShader.sh
@@ -1,5 +1,5 @@
-/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
-/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
+/Applications/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
+/Applications/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
 
-/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/x64-debug/LyraGui/data/shader/vert.spv
-/Application/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/x64-debug/LyraGui/data/shader/frag.spv
+/Applications/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/macos-debug/LyraGui/data/shader/vert.spv
+/Applications/Libraries/VulkanSDK/macOS/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/macos-debug/LyraGui/data/shader/frag.spv
