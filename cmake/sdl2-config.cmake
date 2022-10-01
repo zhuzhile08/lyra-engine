49b744a75f3f266bdb5d033ce8f8de6db6144298
zhuzhile08 <zhuzhile08@gmail.com>
2022-10-01 22:42:54 +0200

    minor changes and fixes

--- cmake/sdl2-config.cmake
+++ cmake/sdl2-config.cmake
@@ -10,5 +10,6 @@ if(WIN32)
 elseif(APPLE)
 	set(SDL2_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/include/SDL2/")
 
-	set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/libSDL2.a;${CMAKE_SOURCE_DIR}/lib/libSDL2main.a")
+	# set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/libSDL2.a;${CMAKE_SOURCE_DIR}/lib/libSDL2main.a")
+	set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/libSDL2-2.0.dylib;${CMAKE_SOURCE_DIR}/lib/libSDL2main.a")
 endif()
