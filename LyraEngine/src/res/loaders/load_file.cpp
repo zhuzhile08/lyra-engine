5467fcfd6ecdd7eaf268c1bed81f357a8d0f6402
zhuzhile08 <zhuzhile08@gmail.com>
2022-10-09 11:00:19 +0200

    load files via the absolute path

--- LyraEngine/src/res/loaders/load_file.cpp
+++ LyraEngine/src/res/loaders/load_file.cpp
@@ -1,12 +1,19 @@
 #include <res/loaders/load_file.h>
 
+#include <filesystem>
+
+#include <core/util.h>
+
 namespace lyra {
 
 namespace util {
 
 void load_file(const char* path, const int mode, std::ifstream& file) {
+	// get the absolute path of the relative path
+	std::filesystem::path absPath(get_executable_path() / path);
+
 	// load the binary
-	file.open(path, static_cast<std::ios_base::openmode>(mode));
+	file.open(absPath, static_cast<std::ios_base::openmode>(mode));
 	lassert(file.good(), "Failed to open a file for reading at path: ", path, "!");
 	Logger::log_debug(Logger::tab(), "Successfully loaded file in at path ", path, "!");
 }
