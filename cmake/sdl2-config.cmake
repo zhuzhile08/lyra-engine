e80531cc61822b3345f8d8dff7228ea9c082519f
zhuzhile08 <zhuzhile08@gmail.com>
2022-10-10 08:36:36 +0200

    remove code repetition

--- cmake/sdl2-config.cmake
+++ cmake/sdl2-config.cmake
@@ -1,6 +1,6 @@
-if(WIN32)
-	set(SDL2_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/include/SDL/")
+set(SDL2_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/include/SDL2/")
 
+if(WIN32)
 	# Support both 32 and 64 bit builds
 	if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
 		set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/x64/SDL2.lib;${CMAKE_SOURCE_DIR}/lib/x64/SDL2main.lib")
@@ -8,8 +8,6 @@ if(WIN32)
 		set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/x86/SDL2.lib;${CMAKE_SOURCE_DIR}/lib/x86/SDL2main.lib")
 	endif ()
 elseif(APPLE)
-	set(SDL2_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/include/SDL2/")
-
 	# set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/libSDL2.a;${CMAKE_SOURCE_DIR}/lib/libSDL2main.a")
 	set(SDL2_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/libSDL2-2.0.dylib;${CMAKE_SOURCE_DIR}/lib/libSDL2main.a")
 endif()
