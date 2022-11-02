2b88db05bc72da24d33e9d42e7d0b974b685c4bf
Zhile <zhuzhile08@gmail.com>
2022-11-02 20:23:43 +0100

    open the file per relative path on windows

--- LyraEngine/src/res/loaders/load_file.cpp
+++ LyraEngine/src/res/loaders/load_file.cpp
@@ -8,10 +8,14 @@ namespace util {
 
 void load_file(const char* path, const int& mode, std::ifstream& file) {
 	// get the absolute path of the relative path
+#ifndef _WIN32
 	std::filesystem::path absPath(get_executable_path() / path);
 
 	// load the binary
 	file.open(absPath, static_cast<std::ios_base::openmode>(mode));
+#else
+	file.open(path, static_cast<std::ios_base::openmode>(mode));
+#endif
 	lassert(file.good(), "Failed to open a file for reading at path: ", path, "!");
 	Logger::log_debug(Logger::tab(), "Successfully loaded file in at path ", path, "!");
 }
