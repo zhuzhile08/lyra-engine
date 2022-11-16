37db48e800667d546c15866c7d776e34ff2edb22
zhuzhile08 <zhuzhile08@gmail.com>
2022-11-16 19:54:41 +0100

    properly implemented cubemaps and skyboxes

--- LyraEngine/include/nodes/graphics/cubemap.h
+++ LyraEngine/include/nodes/graphics/cubemap.h
@@ -11,30 +11,106 @@
 
 #pragma once
 
-#include <nodes/spatial.h>
+#include <array>
 
-#include <rendering/texture.h>
+#include <rendering/vulkan/descriptor.h>
+#include <rendering/vulkan/vulkan_image.h>
+#include <rendering/vulkan/GPU_memory.h>
+#include <rendering/graphics_pipeline.h>
+
 #include <nodes/mesh/mesh.h>
 #include <nodes/mesh/mesh_renderer.h>
 
-#include <array>
-
 namespace lyra {
 
-/**
-class Cubemap : public Spatial {
-public:
-	 * @brief construct a new cubemap
-	 * 
-	 * @param path paths of the images for the cubemap
+// cubemap base class
+class CubemapBase : private vulkan::Image, private vulkan::GPUMemory, public GraphicsPipeline {
+public:	
+	CubemapBase(
+		const std::array<const char*, 6>& paths,
+		const char* vertexShaderPath,
+		const char* fragShaderPath,
+		Camera* const camera,
+		const VkFormat& format,
+		const ColorBlending& colorBlending,
+		const Tessellation& tessellation,
+		const Multisampling& multisampling
+	);
 
-	Cubemap(std::array<const char*, 6> path) { }
+	/**
+	 * @brief draw the cubemap
+	 * 
+	 */
+	void draw() const;
+
+	/**
+	 * @brief get the information to bind to a descriptor
+	 * 
+	 * @param layout image layout
+	 * 
+	 * @return constexpr VkDescriptorImageInfo
+	*/
+	NODISCARD constexpr VkDescriptorImageInfo get_descriptor_cubemap_info(const VkImageLayout& layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) const noexcept {
+		return {
+			m_sampler,
+			m_view,
+			layout
+		};
+	}
+
+	/**
+	 * @brief get the image
+	 * 
+	 * @return constexpr VkImageView
+	*/
+	NODISCARD constexpr VkImageView view() const noexcept { return m_view; }
+	/**
+	 * @brief get the sampler
+	 * 
+	 * @return consexprt VkSampler
+	*/
+	NODISCARD constexpr VkSampler sampler() const noexcept { return m_sampler; }
+	/**
+	 * @brief get the memory
+	 * 
+	 * @return constexpr VmaAllocation
+	*/
+	NODISCARD constexpr VmaAllocation memory() const noexcept { return m_memory; }
 
 private:
-	std::array<Texture, 6> textures;
-	// Mesh cube;
-	// MeshRenderer cubeRenderer;
+	VkSampler m_sampler;
+
+	std::vector<vulkan::Descriptor> m_descriptors;
+	Mesh m_cubeMesh;
+	MeshRenderer m_cubeMeshRenderer;
 };
-*/
+
+// cubemap
+class Cubemap : public CubemapBase {
+public:
+	Cubemap(
+		const std::array<const char*, 6>& paths,
+		Script* script,
+		Camera* const camera,
+		const VkFormat& format = VK_FORMAT_R8G8B8A8_SRGB,
+		const ColorBlending& colorBlending = ColorBlending::BLEND_ENABLE,
+		const Tessellation& tessellation = Tessellation::TESSELLATION_ENABLE,
+		const Multisampling& multisampling = Multisampling::MULTISAMPLING_ENABLE
+	) : CubemapBase(paths, "data/shader/cubemapVert.spv", "data/shader/cubemapFrag.spv", camera, format, colorBlending, tessellation, multisampling) { }
+};
+
+// skybox
+class Skybox : public CubemapBase {
+public:
+	Skybox(
+		const std::array<const char*, 6>& paths,
+		Camera* const camera,
+		const VkFormat& format = VK_FORMAT_R8G8B8A8_SRGB,
+		const ColorBlending& colorBlending = ColorBlending::BLEND_ENABLE,
+		const Tessellation& tessellation = Tessellation::TESSELLATION_ENABLE,
+		const Multisampling& multisampling = Multisampling::MULTISAMPLING_ENABLE
+	) : CubemapBase(paths, "data/shader/skyboxVert.spv", "data/shader/skyboxFrag.spv", camera, format, colorBlending, tessellation, multisampling) 
+	{ camera->m_skybox = this; }
+};
 
 } // namespace lyra
--- LyraEngine/src/nodes/graphics/cubemap.cpp
+++ LyraEngine/src/nodes/graphics/cubemap.cpp
@@ -1,30 +1,184 @@
 #include <nodes/graphics/cubemap.h>
 
+#include <core/settings.h>
+
+#include <rendering/assets.h>
+
+#include <rendering/vulkan/vulkan_shader.h>
+#include <rendering/vulkan/vulkan_pipeline.h>
+#include <rendering/vulkan/GPU_buffer.h>
+
+#include <nodes/graphics/camera.h>
+
 namespace lyra {
 
-/** 
-Cubemap::Cubemap(std::array<const char*, 6> path) : 
-	cube({ 
-		{ - 1.0f, -1.0f,  1.0f },
-		{ 1.0f, -1.0f,  1.0f },
-		{ 1.0f, -1.0f, -1.0f },
-		{ -1.0f, -1.0f, -1.0f },
-		{ -1.0f,  1.0f,  1.0f },
-		{ 1.0f,  1.0f,  1.0f },
-		{ 1.0f,  1.0f, -1.0f },
-		{ -1.0f,  1.0f, -1.0f  } }, 
-		{ 
-			1, 2, 6, 6, 5, 1,
-			0, 4, 7, 7, 3, 0,
-			4, 5, 6,6, 7, 4,
-			0, 3, 2,2, 1, 0,
-			0, 1, 5,5, 4, 0,
-			3, 7, 6,6, 2, 3 
-		}
-) {
+CubemapBase::CubemapBase(
+	const std::array<const char*, 6>& paths,
+	const char* vertexShaderPath,
+	const char* fragShaderPath,
+	Camera* const camera,
+	const VkFormat& format,
+	const ColorBlending& colorBlending,
+	const Tessellation& tessellation,
+	const Multisampling& multisampling
+) : 
+	GraphicsPipeline(
+		camera, 
+		{
+			{ vulkan::Shader::Type::TYPE_VERTEX, vertexShaderPath, "main" },
+			{ vulkan::Shader::Type::TYPE_FRAGMENT, fragShaderPath, "main" }
+		}, 
+		{
+			{ 0, vulkan::Descriptor::Type::TYPE_UNIFORM_BUFFER, Settings::Rendering::maxFramesInFlight, vulkan::Shader::Type::TYPE_VERTEX },
+			{ 0, vulkan::Descriptor::Type::TYPE_IMAGE_SAMPLER, Settings::Rendering::maxFramesInFlight, vulkan::Shader::Type::TYPE_FRAGMENT }
+		}, 
+		{},
+		colorBlending,
+		tessellation,
+		multisampling
+		),
+	m_cubeMesh(
+		{
+			Mesh::Vertex({-1.0f,	-1.0f,	1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex({1.0f, 	-1.0f,  1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex({1.0f, 	-1.0f, 	-1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex({-1.0f, 	-1.0f, 	-1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex({-1.0f,	1.0f,  	1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex({1.0f,  	1.0f,  	1.0f}, glm::vec3(), glm::vec2()),
+			Mesh::Vertex{{1.0f,  	1.0f, 	-1.0f}, glm::vec3(), glm::vec2()},
+			Mesh::Vertex({-1.0f, 	1.0f, 	-1.0f}, glm::vec3(), glm::vec2())
+		},
+		{
+			1, 2, 6,
+			6, 5, 1,
+			0, 4, 7,
+			7, 3, 0,
+			4, 5, 6,
+			6, 7, 4,
+			0, 3, 2,
+			2, 1, 0,
+			0, 1, 5,
+			5, 4, 0,
+			3, 7, 6,
+			6, 2, 3
+		},
+		nullptr
+	),
+	m_cubeMeshRenderer(
+		&m_cubeMesh, nullptr
+	)
+{
+	{ // stuff for creating images
+		// load all the images raw first
+		std::array<Assets::ImageData, 6> imageData;
+		imageData[0] = Assets::unpack_texture(paths[0]);
+		imageData[1] = Assets::unpack_texture(paths[1]);
+		imageData[2] = Assets::unpack_texture(paths[2]);
+		imageData[3] = Assets::unpack_texture(paths[3]);
+		imageData[4] = Assets::unpack_texture(paths[4]);
+		imageData[5] = Assets::unpack_texture(paths[5]);
 
+		// get the size of one of the images for future use
+		auto width = imageData[0].width, height = imageData[0].height;
+		// extent (size) of the image
+		VkExtent3D imageExtent = {width, height, 1};
+		
+		const void* combinedImageData[6] = {
+			imageData[0].data,
+			imageData[1].data,
+			imageData[2].data,
+			imageData[3].data,
+			imageData[4].data,
+			imageData[5].data
+		};
+
+		// create the staging buffer
+		vulkan::GPUBuffer stagingBuffer(width * height * 4 * 6, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_CPU_ONLY);
+		// copy the image data into the staging buffer
+		stagingBuffer.copy_data(combinedImageData, 6, width * height * 4);
+
+		// create the image
+		vassert(Application::renderSystem()->device()->createImage(
+			get_image_create_info(
+				format,
+				imageExtent,
+				VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+				1, 
+				VK_IMAGE_TYPE_2D,
+				6,
+				VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT
+			),
+			get_alloc_create_info(VMA_MEMORY_USAGE_GPU_ONLY),
+			m_image, 
+			m_memory), "create cubemap texture(s)");
+
+		// transition the layout of the image
+		transition_layout(VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_FORMAT_R8G8B8A8_SRGB, {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 6});
+		// copy the buffer contents into the image
+		copy_from_buffer(&stagingBuffer, imageExtent);
+		
+		// finally, create the image view
+		create_view(format, { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 6 }, VK_IMAGE_VIEW_TYPE_CUBE);
+	}
+
+	{ // create a sampler for the image
+		VkPhysicalDeviceProperties properties;
+		vkGetPhysicalDeviceProperties(Application::renderSystem()->device()->physicalDevice(), &properties);
+
+		VkSamplerCreateInfo samplerInfo {
+			VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
+			nullptr,
+			0,
+			VK_FILTER_LINEAR,
+			VK_FILTER_LINEAR,
+			VK_SAMPLER_MIPMAP_MODE_LINEAR,
+			VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,
+			VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,
+			VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,
+			0.0f,
+			VK_FALSE,
+			1.0f, // properties.limits.maxSamplerAnisotropy * Settings::Rendering::anistropy,
+			VK_FALSE,
+			VK_COMPARE_OP_ALWAYS,
+			0.0f,
+			0.0f,
+			VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK,
+			VK_FALSE
+		};
+
+		vassert(vkCreateSampler(Application::renderSystem()->device()->device(), &samplerInfo, nullptr, &m_sampler), "create Vulkan image sampler!");
+	}
+
+	{ // next, create the descriptors
+		// write the bindings
+		vulkan::Descriptor::Writer writer;
+		writer.add_writes({
+			{ get_descriptor_cubemap_info(), 1, vulkan::Descriptor::Type::TYPE_IMAGE_SAMPLER}
+		});
+		writer.add_writes({
+			{ camera->m_buffers.at(0).get_descriptor_buffer_info(), 0, lyra::vulkan::Descriptor::Type::TYPE_UNIFORM_BUFFER },
+			{ camera->m_buffers.at(1).get_descriptor_buffer_info(), 0, lyra::vulkan::Descriptor::Type::TYPE_UNIFORM_BUFFER }
+		});
+
+		// create both descriptors
+		for (uint32 i = 0; i < Settings::Rendering::maxFramesInFlight; i++) 
+			m_descriptors.emplace_back(
+				m_descriptorSetLayout.get(),
+				0,
+				m_descriptorPool.get(), 
+				writer
+			);
+	}
 }
 
- */
+void CubemapBase::draw() const {
+	Application::renderSystem()->currentCommandBuffer().bindPipeline(bindPoint(), pipeline());
+	Application::renderSystem()->currentCommandBuffer().bindDescriptorSet(
+		bindPoint(), 
+		layout(),
+		0, 
+		m_descriptors[Application::renderSystem()->currentFrame()].get());
+	m_cubeMeshRenderer.draw();
+}
 
 } // namespace lyra
