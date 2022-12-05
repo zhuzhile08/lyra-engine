0ba73b6ff828c35c5838f8dfc62cc78951684722
zhuzhile08 <zhuzhile08@gmail.com>
2022-12-05 11:39:52 +0100

    rework mesh loading to load only the entire file

--- LyraEngine/src/nodes/mesh/mesh.cpp
+++ LyraEngine/src/nodes/mesh/mesh.cpp
@@ -34,8 +34,8 @@ const std::array<VkVertexInputAttributeDescription, 4> Mesh::Vertex::get_attribu
 		{
 			3,
 			0,
-			VK_FORMAT_R32G32_SFLOAT,
-			offsetof(Vertex, uv)
+			VK_FORMAT_R32G32B32_SFLOAT,
+			offsetof(Vertex, uvw)
 		}}
 	};
 }
@@ -44,18 +44,13 @@ const std::array<VkVertexInputAttributeDescription, 4> Mesh::Vertex::get_attribu
 Mesh::Mesh(
 	const char* path, 
 	Script* script,
-	const uint16& index,
 	const char* name,
 	Spatial* parent,
 	const bool& visible,
 	const uint32& tag,
 	const Transform& transform
 ) : Spatial(nullptr, name, parent, visible, tag, transform) {
-	Logger::log_info("Creating Mesh... ");
-
-	create_mesh(util::load_model(path), index);
-
-	Logger::log_info("Successfully created mesh at address: ", get_address(this), "with path: ", path, "!", Logger::end_l());
+	create_mesh(util::load_model(path));
 }
 
 Mesh::Mesh(
@@ -69,75 +64,39 @@ Mesh::Mesh(
 	const Transform& transform
 ) : Spatial(nullptr, name, parent, visible, tag, transform), m_vertices(vertices), m_indices(indices) { }
 
-void Mesh::create_mesh(const util::LoadedModel& loaded, const uint16& index) {
+void Mesh::create_mesh(const util::LoadedModel& loaded) {
 	// this is, as far as I know, veeeeeery inefficient, but I lack the knowlege to make it better, I don't even understand what is going on
 	// @todo make some sort of application that loads models into a text file that this engine can read muuuuuuuuuuuch faster and easier but for now, I'll stick to this
 
-	switch (index) {
-
-	case UINT16_MAX: // default option: load everything
-		// loop over all the shapes
-		for (uint32 s = 0; s < loaded.shapes.size(); s++) {
-			// loop over all the polygons
-			for (uint32 i = 0; i < loaded.shapes.at(s).mesh.indices.size(); i++) {
-				Vertex vertex;
-
-				// calculate positions
-				vertex.pos = {
-					loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 0),
-					loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 1),
-					loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 2)
-				};
-				// calculate normals
-				vertex.normal = {
-					loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 0),
-					loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 1),
-					loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 2)
-				};
-				// calculate UV coordinates
-				vertex.uv = {
-					loaded.vertices.texcoords.at(2 * loaded.shapes.at(s).mesh.indices.at(i).texcoord_index + 0),
-					1.0f - loaded.vertices.texcoords.at(2 * loaded.shapes.at(s).mesh.indices.at(i).texcoord_index + 1)
-				};
-
-				// add the vertex to the list
-				m_vertices.push_back(vertex);
-				m_indices.push_back(static_cast<uint32>(m_indices.size()));
-			}
-		} 
-
-		break;
-
-	default: // funnily enough, this is not the default option
+	// loop over all the shapes
+	for (uint32 s = 0; s < loaded.shapes.size(); s++) {
 		// loop over all the polygons
-		for (uint32 i = 0; i < loaded.shapes.at(index).mesh.indices.size(); i++) {
+		for (uint32 i = 0; i < loaded.shapes.at(s).mesh.indices.size(); i++) {
 			Vertex vertex;
 
 			// calculate positions
 			vertex.pos = {
-				loaded.vertices.vertices.at(3 * loaded.shapes.at(index).mesh.indices.at(i).vertex_index + 0),
-				loaded.vertices.vertices.at(3 * loaded.shapes.at(index).mesh.indices.at(i).vertex_index + 1),
-				loaded.vertices.vertices.at(3 * loaded.shapes.at(index).mesh.indices.at(i).vertex_index + 2)
+				loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 0),
+				loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 1),
+				loaded.vertices.vertices.at(3 * loaded.shapes.at(s).mesh.indices.at(i).vertex_index + 2)
 			};
 			// calculate normals
 			vertex.normal = {
-				loaded.vertices.normals.at(3 * loaded.shapes.at(index).mesh.indices.at(i).normal_index + 0),
-				loaded.vertices.normals.at(3 * loaded.shapes.at(index).mesh.indices.at(i).normal_index + 1),
-				loaded.vertices.normals.at(3 * loaded.shapes.at(index).mesh.indices.at(i).normal_index + 2)
+				loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 0),
+				loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 1),
+				loaded.vertices.normals.at(3 * loaded.shapes.at(s).mesh.indices.at(i).normal_index + 2)
 			};
 			// calculate UV coordinates
-			vertex.uv = {
-				loaded.vertices.texcoords.at(2 * loaded.shapes.at(index).mesh.indices.at(i).texcoord_index + 0),
-				1.0f - loaded.vertices.texcoords.at(2 * loaded.shapes.at(index).mesh.indices.at(i).texcoord_index + 1)
+			vertex.uvw = {
+				loaded.vertices.texcoords.at(2 * loaded.shapes.at(s).mesh.indices.at(i).texcoord_index + 0),
+				1.0f - loaded.vertices.texcoords.at(2 * loaded.shapes.at(s).mesh.indices.at(i).texcoord_index + 1),
+				1.0f
 			};
 
 			// add the vertex to the list
 			m_vertices.push_back(vertex);
 			m_indices.push_back(static_cast<uint32>(m_indices.size()));
 		}
-
-		break;
-
 	}
 }
 
