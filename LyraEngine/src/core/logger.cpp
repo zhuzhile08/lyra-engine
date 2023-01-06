190119d8dc0c91abbc1007b65b863399cef4f64e
Zhile <zhuzhile08@gmail.com>
2023-01-06 21:13:15 +0100

    move the terminal init function to logger to avoid static init

--- LyraEngine/src/core/logger.cpp
+++ LyraEngine/src/core/logger.cpp
@@ -1,6 +1,29 @@
 #include <core/logger.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 namespace lyra {
+
+Logger::Logger() :
+	m_logFile("data/log/log.txt", std::ofstream::out | std::ofstream::trunc)
+{
+	// create a virtual console if on windows
+#ifndef STDIO_SYNC
+	std::ios::sync_with_stdio(true);
+#endif
+
+#ifdef _WIN32
+	DWORD outMode = 0;
+	HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleMode(stdoutHandle, &outMode);
+	outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+	SetConsoleMode(stdoutHandle, outMode);
+#endif
+};
 
 void Logger::clear_buffer() {
 	set_color_default();
