837b84c4fc702db561c1d75f1d8a86fed64957ec
zhuzhile08 <zhuzhile08@gmail.com>
2023-01-14 12:42:11 +0100

    continue implementation

--- LyraEngine/include/core/dynarray.h
+++ LyraEngine/include/core/dynarray.h
@@ -11,14 +11,22 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include <core/array.h>
+
+#define NODISCARD [[nodiscard]]
 
 namespace lyra {
 
+// allowed dynamic array internal type
+template <typename Type>
+concept DynarrayValueType = std::is_default_constructible_v<Type> && std::is_copy_assignable_v<Type> && std::is_default_constructible_v<Type>;
+
 // very dangerous dynamic array implementation, only store contents <= 4 bytes with less than 16 capacity
-template <class Ty, size_t capacity> class Dynarray {
+template <DynarrayValueType Ty, size_t capacity> class Dynarray {
 public:
-    typedef Ty value_type;
+    typedef DynarrayValueType value_type;
     typedef size_t size_type;
     typedef Array<value_type, capacity> array_type;
     typedef Dynarray<Ty, capacity> wrapper_type;
@@ -27,7 +35,8 @@ public:
      * @brief copy assignment operator
      * 
      * @param right the array to copy from
-     * @return wrapper_type& 
+     * 
+     * @return lyra::Dynarray::wrapper_type& 
      */
     wrapper_type& operator=(const wrapper_type& right) {
         for (size_t i; i < m_size; i++) m_array[i] = right.m_array[i];
@@ -77,6 +86,42 @@ public:
     }
 
     /**
+     * @brief clear all the members in the array and set size to 0
+     */
+    void clear() {
+        m_array.fill(Ty());
+        m_size = 0;
+    }
+    
+    void insert(const size_t& index, const value_type& value) { 
+
+    }
+    void insert(const size_t&index, value_type&& value) {
+
+    }
+    void insert(const size_t& begin, const size_t& end, const value_type& value) {
+
+    }
+    void insert(const size_t& begin, const size_t& end, value_type&& value) {
+
+    }
+    /**
+     * @brief remove the last element in the array
+     */
+    void pop_back() {
+        end() = Ty();
+        m_size--;
+    }
+    /**
+     * @brief resize the array
+     * 
+     * @param size new size of the array
+     */
+    void resize(const size_t& size) {
+        if (m_size <= size) m_size = size;
+        else for ( ; m_size > size; m_size--) pop_back();
+    }
+    /**
      * @brief swap the contents of the array with another array
      * 
      * @param array array to swap with
@@ -88,33 +133,33 @@ public:
     /**
      * @brief get the first element of the array
      * 
-     * @return constexpr Ty&
+     * @return constexpr lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr Ty& begin() noexcept {
+    NODISCARD constexpr value_type& begin() noexcept {
         return m_array[0];
     }
     /**
      * @brief get the first element of the array
      * 
-     * @return constexpr Ty& const
+     * @return constexpr lyra::Dynarray::value_type& const
      */
-    NODISCARD constexpr const Ty& begin() const noexcept {
+    NODISCARD constexpr const value_type& begin() const noexcept {
         return m_array[0];
     }
     /**
      * @brief get the last element of the array
      * 
-     * @return constexpr Ty&
+     * @return constexpr lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr Ty& end() noexcept {
+    NODISCARD constexpr value_type& end() noexcept {
         return m_array[m_size - 1];
     }
     /**
      * @brief get the last element of the array
      * 
-     * @return constexpr const Ty& 
+     * @return constexpr const lyra::Dynarray::value_type& 
      */
-    NODISCARD constexpr const Ty& end() const noexcept {
+    NODISCARD constexpr const value_type& end() const noexcept {
         return m_array[m_size - 1];
     }
 
@@ -122,27 +167,27 @@ public:
      * @brief get an element of the array
      * 
      * @param index index of the element
-     * @return constexpr Ty&
+     * @return constexpr lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr Ty& operator[](const size_t& index) noexcept {
+    NODISCARD constexpr value_type& operator[](const size_t& index) noexcept {
         return m_array[index];
     }
     /**
      * @brief get an element of the array
      * 
      * @param index index of the element
-     * @return constexpr const Ty&
+     * @return constexpr const lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr const Ty& operator[](const size_t& index) const noexcept {
+    NODISCARD constexpr const value_type& operator[](const size_t& index) const noexcept {
         return m_array[index];
     }
     /**
      * @brief get an element of the array with no UB posibility
      * 
      * @param index index of the element
-     * @return constexpr Ty&
+     * @return constexpr lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr Ty& at(const size_t& index) noexcept {
+    NODISCARD constexpr value_type& at(const size_t& index) noexcept {
         if (index >= m_size) return m_array[m_size - 1];
         return m_array[index];
     }
@@ -150,9 +195,9 @@ public:
      * @brief get an element of the array with no UB posibility
      * 
      * @param index index of the element
-     * @return constexpr const Ty&
+     * @return constexpr const lyra::Dynarray::value_type&
      */
-    NODISCARD constexpr const Ty& at(const size_t& index) const noexcept {
+    NODISCARD constexpr const value_type& at(const size_t& index) const noexcept {
         if (index >= m_size) return m_array[m_size - 1];
         return m_array[index];
     }
@@ -166,6 +211,14 @@ public:
         return m_size;
     }
     /**
+     * @brief get the capacity of the internal array
+     * 
+     * @return constexpr const size_t
+     */
+    NODISCARD constexpr const size_t max_size() const noexcept {
+        return Size;
+    }
+    /**
      * @brief check if the array is sized 0
      * 
      * @return constexpr const bool
@@ -177,30 +230,32 @@ public:
     /**
      * @brief get the raw array
      * 
-     * @return constexpr value_type*
+     * @return constexpr lyra::Dynarray::value_type*
      */
-    NODISCARD constexpr value_type* data() noexcept { return m_array; }
+    NODISCARD constexpr value_type* data() noexcept { 
+        return m_array; 
+    }
     /**
      * @brief get the raw array
      * 
-     * @return constexpr const value_type*
+     * @return constexpr const lyra::Dynarray::value_type*
      */
     NODISCARD constexpr const value_type* data() const noexcept { return m_array; }
     /**
      * @brief cast the wrapper to the raw array
      * 
-     * @return constexpr value_type*
+     * @return constexpr lyra::Dynarray::value_type*
      */
     NODISCARD constexpr operator value_type* () noexcept { return m_array; }
     /**
      * @brief cast the wrapper to the raw array
      * 
-     * @return constexpr const value_type*
+     * @return constexpr const lyra::Dynarray::value_type*
      */
     NODISCARD constexpr operator const value_type* () const noexcept { return m_array; }
 
 private:
-    size_type m_size;
+    size_type m_size = 0;
     array_type m_array;
 };
 
