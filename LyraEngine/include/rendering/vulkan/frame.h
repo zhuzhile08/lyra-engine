10a0b7234fced37ddbab96947dc37160fecaada7
zhuzhile08 <zhuzhile08@gmail.com>
2023-02-06 11:26:44 +0100

    convert spaces to tabs

--- LyraEngine/include/rendering/vulkan/frame.h
+++ LyraEngine/include/rendering/vulkan/frame.h
@@ -26,68 +26,68 @@ namespace vulkan {
  */
 class Frame {
 public:
-    /**
-     * @brief construct the frame
-     */
-    Frame() : m_commandBuffer(CommandBuffer::Usage::USAGE_RENDERING_DEFAULT) {
-        create_sync_objects();
-    }
-    /**
-     * @brief destruct the frame
-     */
-    virtual ~Frame();
-    /**
-     * @brief recreate the frame
-     *
-     * @param index index of the frame in the renderer
-     */
-    void recreate();
-    
-    /**
-     * @brief wait for Vulkan fences
-     */
-    void wait() const;
-    /**
-     * @brief reset Vulkan fences
-     */
-    void reset() const;
+	/**
+	 * @brief construct the frame
+	 */
+	Frame() : m_commandBuffer(CommandBuffer::Usage::USAGE_RENDERING_DEFAULT) {
+		create_sync_objects();
+	}
+	/**
+	 * @brief destruct the frame
+	 */
+	virtual ~Frame();
+	/**
+	 * @brief recreate the frame
+	 *
+	 * @param index index of the frame in the renderer
+	 */
+	void recreate();
+	
+	/**
+	 * @brief wait for Vulkan fences
+	 */
+	void wait() const;
+	/**
+	 * @brief reset Vulkan fences
+	 */
+	void reset() const;
 
-    /** 
-     * @brief get the image available semaphores
-     *
-     * @return const VkSemaphor&
-    */
-    NODISCARD const VkSemaphore& imageAvailableSemaphores() const noexcept { return m_imageAvailableSemaphore; }
-    /**
-     * @brief get the render finished semaphores
-     *
-     * @return const VkSemaphore&
-    */
-    NODISCARD const VkSemaphore& renderFinishedSemaphores() const noexcept { return m_renderFinishedSemaphore; }
-    /**
-     * @brief get the in flight fences
-     *
-     * @return const VkFence&
-    */
-    NODISCARD const VkFence& inFlightFences() const noexcept { return m_inFlightFence; }
-    /**
-     * @brief get the in flight fences
-     *
-     * @return constexpr const CommandBuffer&
-    */
-    NODISCARD constexpr const CommandBuffer& commandBuffer() const noexcept { return m_commandBuffer; }
+	/** 
+	 * @brief get the image available semaphores
+	 *
+	 * @return const VkSemaphor&
+	*/
+	NODISCARD const VkSemaphore& imageAvailableSemaphores() const noexcept { return m_imageAvailableSemaphore; }
+	/**
+	 * @brief get the render finished semaphores
+	 *
+	 * @return const VkSemaphore&
+	*/
+	NODISCARD const VkSemaphore& renderFinishedSemaphores() const noexcept { return m_renderFinishedSemaphore; }
+	/**
+	 * @brief get the in flight fences
+	 *
+	 * @return const VkFence&
+	*/
+	NODISCARD const VkFence& inFlightFences() const noexcept { return m_inFlightFence; }
+	/**
+	 * @brief get the in flight fences
+	 *
+	 * @return constexpr const CommandBuffer&
+	*/
+	NODISCARD constexpr const CommandBuffer& commandBuffer() const noexcept { return m_commandBuffer; }
 
 private:
-    VkSemaphore m_imageAvailableSemaphore;
-    VkSemaphore m_renderFinishedSemaphore;
-    VkFence m_inFlightFence;
+	VkSemaphore m_imageAvailableSemaphore;
+	VkSemaphore m_renderFinishedSemaphore;
+	VkFence m_inFlightFence;
 
-    CommandBuffer m_commandBuffer;
+	CommandBuffer m_commandBuffer;
 
-    /**
-     * @brief create the syncronisation objects
-     */
-    void create_sync_objects();
+	/**
+	 * @brief create the syncronisation objects
+	 */
+	void create_sync_objects();
 };
 
 } // namespace vulkan
