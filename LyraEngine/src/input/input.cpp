791859d6ea18a77e640d0ad1588ed6a7d8e8960c
Zhile Zhu <zhuzhile08@gmail.com>
2023-03-17 20:54:04 +0100

    sepearte input enums and inline input manager

--- LyraEngine/src/input/input.cpp
+++ LyraEngine/src/input/input.cpp
@@ -2,44 +2,14 @@
 
 #include <backends/imgui_impl_sdl2.h>
 
+#include <core/application.h>
+
 namespace lyra {
 
-const bool Input::check_key(Keyboard event) {
-	return m_keyboardState[static_cast<SDL_Scancode>(event)];
-}
+namespace input {
 
-const bool Input::check_key_down(Keyboard event) {
-	return m_keyboardState[static_cast<SDL_Scancode>(event)] && !m_prevKeyboardState[static_cast<SDL_Scancode>(event)];
-}
-
-const bool Input::check_key_up(Keyboard event) {
-	return !m_keyboardState[static_cast<SDL_Scancode>(event)] && m_prevKeyboardState[static_cast<SDL_Scancode>(event)];
-}
-
-const bool Input::check_mouse_button(Mouse mouse) {
-	return (m_mouseState & static_cast<uint8>(mouse)) != 0;
-}
-
-const bool Input::check_mouse_button_down(Mouse mouse) {
-	return (m_mouseState & static_cast<uint8>(mouse)) && !(m_prevMouseState & static_cast<uint8>(mouse));
-}
-
-const bool Input::check_mouse_button_up(Mouse mouse) {
-	return !(m_mouseState & static_cast<uint8>(mouse)) && (m_prevMouseState & static_cast<uint8>(mouse));
-}
-
-const glm::ivec2 Input::check_mouse_position() {
-	return m_mousePos;
-}	
-
-void Input::check_mouse_position(glm::ivec2& mousePos) {
-	mousePos = m_mousePos;
-}
-
-void Input::update() {
+void InputManager::update() {
 	// get new events and set the passed events
-	memcpy(m_prevKeyboardState, m_keyboardState, m_keyCount);
-	m_prevMouseState = m_mouseState;
 	SDL_PumpEvents();
 	m_mouseState = SDL_GetMouseState(&m_mousePos.x, &m_mousePos.y);
 
@@ -60,15 +30,14 @@ void Input::update() {
 	}
 }
 
-int Input::m_keyCount;
-const uint8* Input::m_keyboardState = SDL_GetKeyboardState(&Input::m_keyCount);
-uint8* Input::m_prevKeyboardState = new uint8[Input::m_keyCount];
+int InputManager::m_keyCount;
+const uint8* InputManager::m_keyboardState = SDL_GetKeyboardState(&InputManager::m_keyCount);
 
-glm::ivec2 Input::m_mousePos;
+uint32 InputManager::m_mouseState = SDL_GetMouseState(&InputManager::m_mousePos.x, &InputManager::m_mousePos.y);
+glm::ivec2 InputManager::m_mousePos;
 
-uint32 Input::m_mouseState = SDL_GetMouseState(&Input::m_mousePos.x, &Input::m_mousePos.y);
-uint32 Input::m_prevMouseState = m_mouseState;
+SDL_Event InputManager::m_events;
 
-SDL_Event Input::m_events;
+} // namespace input
 
 } // namespace lyra
