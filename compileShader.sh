a0e1a5fdb00803109d44ec853505822862529e21
Zhile Zhu <zhuzhile08@gmail.com>
2023-06-25 09:47:38 +0200

    build shader also in the compute test directory

--- compileShader.sh
+++ compileShader.sh
@@ -4,10 +4,20 @@
 /usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.frag -o data/shader/cubemapFrag.spv
 /usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.vert -o data/shader/skyboxVert.spv
 /usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.frag -o data/shader/skyboxFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/compute.comp -o data/shader/compute.spv
 
-/usr/local/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/Debug/LyraGui/data/shader/vert.spv
-/usr/local/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/Debug/LyraGui/data/shader/frag.spv
-/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.vert -o out/build/Debug/LyraGui/data/shader/cubemapVert.spv
-/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.frag -o out/build/Debug/LyraGui/data/shader/cubemapFrag.spv
-/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.vert -o out/build/Debug/LyraGui/data/shader/skyboxVert.spv
-/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.frag -o out/build/Debug/LyraGui/data/shader/skyboxFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/Debug/Tests/Engine/data/shader/vert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/Debug/Tests/Engine/data/shader/frag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.vert -o out/build/Debug/Tests/Engine/data/shader/cubemapVert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.frag -o out/build/Debug/Tests/Engine/data/shader/cubemapFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.vert -o out/build/Debug/Tests/Engine/data/shader/skyboxVert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.frag -o out/build/Debug/Tests/Engine/data/shader/skyboxFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/compute.comp -o out/build/Debug/Tests/Engine/data/shader/compute.spv
+
+/usr/local/bin/glslc LyraEngine/src/shaders/src/vert.vert -o out/build/Debug/Tests/Compute/data/shader/vert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/frag.frag -o out/build/Debug/Tests/Compute/data/shader/frag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.vert -o out/build/Debug/Tests/Compute/data/shader/cubemapVert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.frag -o out/build/Debug/Tests/Compute/data/shader/cubemapFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.vert -o out/build/Debug/Tests/Compute/data/shader/skyboxVert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.frag -o out/build/Debug/Tests/Compute/data/shader/skyboxFrag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/compute.comp -o out/build/Debug/Tests/Compute/data/shader/compute.spv
