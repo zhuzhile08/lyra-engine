f4a9f8d91fe13150e768a561e205f1a1bbc776b5
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-04 13:01:18 +0800

    Use new json parser

--- LyraEngine/include/Common/Settings.h
+++ LyraEngine/include/Common/Settings.h
@@ -13,10 +13,10 @@
 #pragma once
 
 #include <Common/Common.h>
+#include <Json/Json.h>
 
 #include <vector>
 #include <string>
-#include <nlohmann/json.hpp>
 
 namespace lyra {
 
@@ -41,19 +41,19 @@ public:
 
 	// generell application settings
 	struct AppConfig {
-		const char* description;
-		const int fps;
+		std::string description;
+		int fps;
 	};
 
 	// debug settings
 	struct DebugConfig {
-		const DebugMode debug;
-		const DisableLog disableLog;
-		const bool printFPS;
-		const bool stdioSync;
+		DebugMode debug;
+		DisableLog disableLog;
+		bool printFPS;
+		bool stdioSync;
 
-		const std::vector <const char*> requestedDeviceExtensions;
-		const std::vector <const char*> requestedValidationLayers;
+		std::vector <const char*> requestedDeviceExtensions;
+		std::vector <const char*> requestedValidationLayers;
 	};
 
 	// rendering settings
@@ -62,29 +62,29 @@ public:
 		constexpr static size_t maxSwapchainImages = 8;
 		constexpr static size_t maxConcurrentRenderers = 16;
 
-		const float fov;
+		float32 fov;
 
-		const bool anistropy;
-		const float anistropyStrength;
-		const float resolution;
+		bool anistropy;
+		float32 anistropyStrength;
+		float32 resolution;
 	};
 
 	// window settings
 	struct WindowConfig {
-		const std::string title;
-		const std::string iconPath;
+		std::string title;
+		std::string iconPath;
 
-		const uint32 width; // renderer width and height
-		const uint32 height;
-		const uint32 wWidth; // window width and height
-		const uint32 wHeight;
+		uint32 width; // renderer width and height
+		uint32 height;
+		uint32 wWidth; // window width and height
+		uint32 wHeight;
 		
-		const bool resizable;
-		const bool maximized;
-		const bool borderless;
-		const bool fullscreen;
-		const bool alwaysOnTop;
-		const bool vSync;
+		bool resizable;
+		bool maximized;
+		bool borderless;
+		bool fullscreen;
+		bool alwaysOnTop;
+		bool vSync;
 	};
 
 	// memory alloc config
@@ -92,25 +92,14 @@ public:
 		constexpr static size_t maxDescriptorSetLayouts = 4;
 		constexpr static size_t maxDescriptorTypePerPool = 8;
 
-		const uint32 maxComponentCount;
-		const uint32 maxEntityCount;
+		uint32 maxComponentCount;
+		uint32 maxEntityCount;
 	};
 
 	struct GUIConfig {
 
 	};
 
-private:
-	/**
-	 * @brief load the m_json
-	 * 
-	 * @return std::string
-	 */
-	NODISCARD std::string init_json() const;
-
-	nlohmann::json m_json;
-
-public:
 	AppConfig application;
 	DebugConfig debug;
 	RenderConfig rendering;
@@ -124,11 +113,6 @@ private:
 	friend const Settings& settings();
 };
 
-/**
- * @brief get a static settings object
- * 
- * @return const Settings& 
- */
 const Settings& settings();
 
 } // namespace lyra
