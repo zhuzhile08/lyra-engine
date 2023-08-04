c37324f1a69ecbef9ade7b8ba51fa2cb05aae1c4
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-05 06:32:15 +0800

    Small message formatting isse

--- LyraEngine/src/Common/Benchmark.cpp
+++ LyraEngine/src/Common/Benchmark.cpp
@@ -7,7 +7,7 @@ namespace lyra {
 
 Benchmark::Benchmark() : m_start(std::chrono::high_resolution_clock::now()) { 
 	// this isn't techinally a warning but it should stand out amoung all of the other logs
-	log().warning("A benchmarker was created at address: ", get_address(this)); 
+	log().warning("A benchmarker was created at address: ", get_address(this), "!"); 
 }
 
 // destruct and stop the timer
@@ -17,7 +17,7 @@ Benchmark::~Benchmark() {
 	// calculate time passed since start in miliseconds
 	auto elapsed = std::chrono::time_point_cast<std::chrono::microseconds>(end).time_since_epoch().count() - std::chrono::time_point_cast<std::chrono::microseconds>(m_start).time_since_epoch().count();
 
-	log().warning("The benchmarker at address", get_address(this), " exited its scope with a time of: ", elapsed, " microseconds!");
+	log().warning("The benchmarker at address: ", get_address(this), " exited its scope with a time of: ", elapsed, " microseconds!");
 }
 
 } // namespace lyra
