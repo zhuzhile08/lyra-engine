3eecc521275171a48e7d45387536664e4523808b
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-10 20:32:05 +0200

    Remove some options from Init

--- LyraEngine/include/Lyra/Lyra.h
+++ LyraEngine/include/Lyra/Lyra.h
@@ -18,11 +18,8 @@ namespace lyra {
 
 bool init(const Window& window) {
 	return vulkan::init_render_system({
-		"Lyra Engine",
 		{0, 7, 0},
-		window.get(),
-		{ "VK_LAYER_KHRONOS_validation" },
-		{ "VK_KHR_swapchain", "VK_KHR_portability_subset", "VK_EXT_descriptor_indexing" }
+		window.get()
 	});
 }
 
