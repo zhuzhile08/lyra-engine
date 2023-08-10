7260fde2ab7d1865ebd70053de0b3ffda5393f7f
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-10 11:41:59 +0200

    Completly rewrite logging system

--- LyraEngine/include/Common/Logger.h
+++ LyraEngine/include/Common/Logger.h
@@ -16,193 +16,264 @@
 #pragma once
 
 #include <Common/Common.h>
+#include <Common/Utility.h>
+#include <Common/Config.h>
+#include <Common/Array.h>
+#include <Common/FileSystem.h>
 
-#include <iostream>
-#include <fstream>
-#include <utility>
-
+#include <fmt/core.h>
+#include <fmt/chrono.h>
 #include <vulkan/vulkan.h>
+
+#include <utility>
+#include <mutex>
+#include <chrono>
+
+template <class Type> struct fmt::formatter<Type, std::enable_if_t<std::is_enum<Type>::value, char>> : 
+	fmt::formatter<int> {
+	auto format(const Type& value, fmt::format_context& context) {
+		return fmt::formatter<int>::format(static_cast<int>(value), context);
+	}
+};
 
 namespace lyra {
 
-class Logger {
-private:
-	enum class Font {
-		non = 0,
-		bold = 1,
-		dim = 2,
-		italic = 3,
-		underline = 4,
-		blink = 5
-	};
+namespace ansi {
 
-	enum class Color {
-		black = 30,
-		red = 31,
-		green = 32,
-		yellow = 33,
-		blue = 34,
-		magenta = 35,
-		cyan = 36,
-		grey = 90,
-		gray = grey,
-		backgroundRed = 91,
-		backgroundGreen = 92,
-		backgroundYellow = 93,
-		backgroundBlue = 94,
-		backgroundMagenta = 95,
-		backgroundCyan = 96,
-		white = 97
-	};
-
-	NODISCARD constexpr int color_cast(Color color) {
-		return static_cast<int>(color);
-	}
-	NODISCARD constexpr int font_cast(Font font) {
-		return static_cast<int>(font);
-	}
-
-	void ansi(Font font, Color color) {
-		std::cout << "\033[" << font_cast(font) << ";" << color_cast(color) << "m";
-	}
-	/**
-	 * @brief set the color to default
-	 */
-	void set_color_default() {
-		ansi(Font::non, Color::white);
-	}
-
-public:
-	template <typename ... Args> constexpr void message(Args... message) {
-#ifndef NDEBUG
-		// print the message
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-#endif
-	}
-
-	template <typename ... Args> constexpr void debug(Args... message) {
-#ifndef NDEBUG
-		// set the color and font of the message
-		ansi(Font::non, Color::grey);
-		// print the message
-		std::cout << "[DEBUG]: ";
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		m_logFile << "[DEBUG]: ";
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-		// reset color
-		set_color_default();
-#endif
-	}
-
-	template <typename ... Args> constexpr void info(Args... message) {
-#ifndef NDEBUG
-		// set the color and font of the message
-		ansi(Font::non, Color::green);
-		// print the message
-		std::cout << "[INFO]: ";
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		m_logFile << "[INFO]: ";
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-		// reset color
-		set_color_default();
-#endif
-	}
-
-	template <typename ... Args> constexpr void warning(Args... message) {
-#ifndef NDEBUG
-		// set the color and font of the message
-		ansi(Font::non, Color::yellow);
-		// print the message
-		std::cout << "[WARNING]: ";
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		m_logFile << "[WARNING]: ";
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-		// reset color
-		set_color_default();
-#endif
-	}
-
-	template <typename ... Args> constexpr void error(Args... message) {
-		// set the color and font of the message
-		ansi(Font::non, Color::red);
-		// print the message
-		std::cout << "[ERROR]: ";
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		m_logFile << "[ERROR]: ";
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-		// reset color
-		set_color_default();
-	}
-
-	template <typename ... Args> constexpr void exception(Args... message) {
-		// set the color and font of the message
-		ansi(Font::bold, Color::red);
-		// print the message
-		std::cout << "[EXCEPTION]: ";
-		(std::cout << ... << std::forward<Args>(message)) << end_l();
-#ifdef LYRA_LOG_FILE
-		m_logFile << "[EXCEPTION]: ";
-		(m_logFile << ... << std::forward<Args>(message)) << end_l();
-#endif
-		throw std::runtime_error("lyra::log()::exception(): An exception occured!");
-		// reset color
-		set_color_default();
-	}
-
-	void clear_buffer();
-
-	NODISCARD constexpr const char* const tab() {
-		return "\t";
-	}
-	NODISCARD constexpr const char* const end_l() {
-		return "\n";
-	}
-
-private:
-	std::ofstream m_logFile;
-
-	Logger();
-
-	friend Logger& log();
+enum class Font {
+	none,
+	reset = none,
+	bold,
+	dim,
+	italic,
+	underline,
+	blink
 };
 
-/**
- * @brief return a static instance of the Logger
- * 
- * @return Logger& 
- */
-Logger& log();
+inline constexpr std::string set_style(Font font, uint32 foreground, uint32 background) {
+	if constexpr (config::coloredLog == true)
+		return fmt::format("\033[{};38;5;{};48;5;{}m", static_cast<int>(font), foreground, background);
+	else return "";
+}
+inline constexpr std::string set_style(Font font, uint32 foreground) {
+	if constexpr (config::coloredLog == true)
+		return fmt::format("\033[{};38;5;{}m", static_cast<int>(font), foreground);
+	else return "";
+}
+inline constexpr std::string reset_style() noexcept {
+	if constexpr (config::coloredLog == true)
+		return "\033[0;0;0m";	
+	else return "";
+}
 
-/**
- * @brief custom assert function to check if a condition is false
- * 
- * @tparam Args variadic message template
- * @param condition condition to check if false
- * @param message exception message
- */
-template <typename ... Args> constexpr void lassert(bool condition, Args... message) {
-	if (!condition) (log().exception(message), ...);
+} // namespace ansi
+
+namespace log {
+
+namespace detail {
+
+void add_logger(const Logger& logger);
+Logger* const default_logger();
+
+} // namespace detail
+
+enum class Level {
+	log,
+	trace,
+	debug,
+	info,
+	warning,
+	error,
+	exception
+};
+
+class Logger {
+public:
+	Logger() = default;
+	Logger(FILE* out, FILE* err, std::string_view name) : m_outStream(out), m_errStream(err), m_name(name) { detail::add_logger(*this); }
+	Logger(FILE* stream, std::string_view name) : m_outStream(stream), m_errStream(stream), m_name(name) { detail::add_logger(*this); }
+
+	template <class Format, typename ... Args> constexpr void log(Format&& format, Args&&... message) {
+		fmt::print(m_outStream, fmt::runtime(std::forward<Format>(format)), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void trace(Format&& format, Args&&... message) {
+		log<Level::trace>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void debug(Format&& format, Args&&... message) {
+		log<Level::debug>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void info(Format&& format, Args&&... message) {
+		log<Level::info>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void warning(Format&& format, Args&&... message) {
+		log<Level::warning>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void error(Format&& format, Args&&... message) {
+		log<Level::error>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+	template <class Format, typename ... Args> constexpr void exception(Format&& format, Args&&... message) {
+		log<Level::exception>(std::forward<Format>(format), std::forward<Args>(message)...);
+	}
+
+	template <class Msg> constexpr void log(Msg&& message) {
+		fmt::print(m_outStream, std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void trace(Msg&& message) {
+		log<Level::trace>(std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void debug(Msg&& message) {
+		log<Level::debug>(std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void info(Msg&& message) {
+		log<Level::info>(std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void warning(Msg&& message) {
+		log<Level::warning>(std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void error(Msg&& message) {
+		log<Level::error>(std::forward<Msg>(message));
+	}
+	template <class Msg> constexpr void exception(Msg&& message) {
+		log<Level::exception>(std::forward<Msg>(message));
+	}
+
+	constexpr void new_line() {
+		fmt::print(m_outStream, "\n");
+	}
+	
+	NODISCARD constexpr const std::FILE* const outStream() const noexcept {
+		return m_outStream;
+	}
+	NODISCARD constexpr const std::FILE* const errStream() const noexcept {
+		return m_errStream;
+	}
+	NODISCARD constexpr std::FILE* outStream() noexcept {
+		return m_outStream;
+	}
+	NODISCARD constexpr std::FILE* errStream() noexcept {
+		return m_errStream;
+	}
+	NODISCARD constexpr std::string name() const noexcept {
+		return m_name;
+	}
+
+private:
+	std::FILE* m_outStream = stdout;
+	std::FILE* m_errStream = stderr;
+	std::string m_name;
+
+	template <Level logLevel, class Format, typename ... Args> void log(Format&& fmt, Args&&... message) {
+		if constexpr (static_cast<int>(config::disableLog) < static_cast<int>(logLevel)) {
+			if constexpr (logLevel == Level::trace) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [TRACE]: {}", ansi::set_style(ansi::Font::none, 81), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			} else if constexpr (logLevel == Level::debug) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [DEBUG]: {}", ansi::set_style(ansi::Font::none, 242), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			} else if constexpr (logLevel == Level::info) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [INFO]: {}", ansi::set_style(ansi::Font::none, 40), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			} else if constexpr (logLevel == Level::warning) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [WARNING]: {}", ansi::set_style(ansi::Font::none, 184), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			} else if constexpr (logLevel == Level::error) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [ERROR]: {}", ansi::set_style(ansi::Font::none, 197), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			} else if constexpr (logLevel == Level::exception) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [EXCEPTION]: {}", ansi::set_style(ansi::Font::bold, 124), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), fmt::format(fmt::runtime(std::forward<Format>(fmt)), std::forward<Args>(message)...));
+			}
+		} else return;
+	}
+	template <Level logLevel, class Msg> void log(Msg message) {
+		if constexpr (static_cast<int>(config::disableLog) < static_cast<int>(logLevel)) {
+			if constexpr (logLevel == Level::trace) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [TRACE]: {}", ansi::set_style(ansi::Font::none, 81), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			} else if constexpr (logLevel == Level::debug) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [DEBUG]: {}", ansi::set_style(ansi::Font::none, 242), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			} else if constexpr (logLevel == Level::info) {
+				fmt::print(m_outStream, "{}[{:%Y-%m-%d %H:%M:%S}] [INFO]: {}", ansi::set_style(ansi::Font::none, 40), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			} else if constexpr (logLevel == Level::warning) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [WARNING]: {}", ansi::set_style(ansi::Font::none, 184), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			} else if constexpr (logLevel == Level::error) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [ERROR]: {}", ansi::set_style(ansi::Font::none, 197), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			} else if constexpr (logLevel == Level::exception) {
+				fmt::print(m_errStream, "{}[{:%Y-%m-%d %H:%M:%S}] [EXCEPTION]: {}", ansi::set_style(ansi::Font::bold, 124), fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), std::forward<Msg>(message));
+			}
+		} else return;
+	}
+
+	friend class LoggingContext;
+};
+
+Logger* const get(std::string_view name);
+
+template <class Format, typename ... Args> inline void log(Format&& format, Args&&... message) {
+	detail::default_logger()->log(std::forward<Format>(format), std::forward<Args>(message)...);
 }
-/**
- * @brief Vulkan function assert
- * 
- * @tparam Args variadic message template
- * @param function Vulkan function to check
- * @param purpose Purpose of the function
- */
-template <typename Arg> constexpr void vassert(VkResult function, Arg purpose) {
-	if (function != VkResult::VK_SUCCESS) (log().exception("Vulkan Exception: Failed to ", purpose, " with error code: ", function, "!"));
+template <class Format, typename ... Args> inline void trace(Format&& format, Args&&... message) {
+	detail::default_logger()->trace(std::forward<Format>(format), std::forward<Args>(message)...);
 }
+template <class Format, typename ... Args> inline void debug(Format&& format, Args&&... message) {
+	detail::default_logger()->debug(std::forward<Format>(format), std::forward<Args>(message)...);
+}
+template <class Format, typename ... Args> inline void info(Format&& format, Args&&... message) {
+	detail::default_logger()->info(std::forward<Format>(format), std::forward<Args>(message)...);
+}
+template <class Format, typename ... Args> inline void warning(Format&& format, Args&&... message) {
+	detail::default_logger()->warning(std::forward<Format>(format), std::forward<Args>(message)...);
+}
+template <class Format, typename ... Args> inline void error(Format&& format, Args&&... message) {
+	detail::default_logger()->error(std::forward<Format>(format), std::forward<Args>(message)...);
+}
+template <class Format, typename ... Args> inline void exception(Format&& format, Args&&... message) {
+	detail::default_logger()->exception(std::forward<Format>(format), std::forward<Args>(message)...);	
+}
+
+template <class Msg> inline void log(Msg&& message) {
+	detail::default_logger()->log(std::forward<Msg>(message));
+}
+template <class Msg> inline void trace(Msg&& message) {
+	detail::default_logger()->trace(std::forward<Msg>(message));
+}
+template <class Msg> inline void debug(Msg&& message) {
+	detail::default_logger()->debug(std::forward<Msg>(message));
+}
+template <class Msg> inline void info(Msg&& message) {
+	detail::default_logger()->info(std::forward<Msg>(message));
+}
+template <class Msg> inline void warning(Msg&& message) {
+	detail::default_logger()->warning(std::forward<Msg>(message));
+}
+template <class Msg> inline void error(Msg&& message) {
+	detail::default_logger()->error(std::forward<Msg>(message));
+}
+template <class Msg> inline void exception(Msg&& message) {
+	detail::default_logger()->exception(std::forward<Msg>(message));
+}
+
+inline void new_line() {
+	detail::default_logger()->new_line();
+}
+
+} // namespace log
+
+template <class Msg> constexpr void lyraAssert(bool condition, Msg message) {
+	if (!condition) {
+		log::exception(std::forward<Msg>(message));
+		std::abort();
+	}
+}
+template <class Msg> constexpr void VULKAN_ASSERT(VkResult function, Msg message) {
+	if (function != VkResult::VK_SUCCESS) log::exception("Vulkan Exception: Failed to {} with error code: {}!", std::forward<Msg>(message), function);
+}
+
+template <class Format, typename ... Args> inline void lyraAssert(bool condition, Format&& format, Args&&... message) {
+	if (!condition) {
+		log::exception(format, std::forward<Args>(message)...);
+		std::abort();
+	}
+}
+template <class Format, typename ... Args> inline void VULKAN_ASSERT(VkResult function, Format&& format, Args&&... message) {
+	if (function != VkResult::VK_SUCCESS) log::exception("Vulkan Exception: Failed to {} with error code: {}!", fmt::vformat(format, std::forward<Args>(message)...), function);
+}
+
+#define GET_ASSERT_OVERLOAD(_1, _2, _3, Name, ...) Name
+#define ASSERT(...) GET_ASSERT_OVERLOAD(__VA_ARGS__, lyraAssert, lyraAssert)(__VA_ARGS__)
+#define VULKAN_ASSERT(...) GET_ASSERT_OVERLOAD(__VA_ARGS__, VULKAN_ASSERT, VULKAN_ASSERT)(__VA_ARGS__)
 
 } // namespace lyra
--- LyraEngine/src/Common/Logger.cpp
+++ LyraEngine/src/Common/Logger.cpp
@@ -1,38 +1,74 @@
 #include <Common/Logger.h>
 
+#include <ios>
+#include <unordered_map>
+
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 namespace lyra {
 
-Logger::Logger() :
-	m_logFile("data/log/log.txt", std::ofstream::out | std::ofstream::trunc)
-{
-	// create a virtual console if on windows
-#ifndef STDIO_SYNC
-	std::ios::sync_with_stdio(true);
-#endif
+namespace log {
+
+class LoggingContext {
+public:
+	LoggingContext() {
+		std::ios::sync_with_stdio(true);
 
 #ifdef _WIN32
-	DWORD outMode = 0;
-	HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		DWORD outMode = 0;
+		HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	GetConsoleMode(stdoutHandle, &outMode);
-	outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+		GetConsoleMode(stdoutHandle, &outMode);
+		outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-	SetConsoleMode(stdoutHandle, outMode);
+		SetConsoleMode(stdoutHandle, outMode);
 #endif
+
+		defaultLogger = UniquePointer<Logger>::create();
+	}
+
+	std::mutex defaultLoggerMutex;
+	UniquePointer<Logger> defaultLogger;
+
+	std::mutex loggerMutex;
+	std::unordered_map<std::string, UniquePointer<Logger>> loggers;
 };
 
-void Logger::clear_buffer() {
-	set_color_default();
-	std::cout << std::endl;
+static LoggingContext* globalLoggingContext = nullptr;
+
+Logger* const get(std::string_view name) {
+	if (globalLoggingContext == nullptr) {
+		globalLoggingContext = new LoggingContext();
+	}
+
+	std::lock_guard<std::mutex> guard(globalLoggingContext->loggerMutex);
+	return globalLoggingContext->loggers.find(name.data())->second.get();
 }
 
-Logger& log() {
-	static Logger* m_logger = new Logger();
-	return *m_logger;
+namespace detail {
+
+void add_logger(const Logger& logger) {
+	if (globalLoggingContext == nullptr) {
+		globalLoggingContext = new LoggingContext();
+	}
+
+	globalLoggingContext->loggers.emplace(logger.name(), UniquePointer<Logger>::create(logger));
 }
+
+Logger* const default_logger() {
+	if (globalLoggingContext == nullptr) {
+		globalLoggingContext = new LoggingContext();
+	}
+
+	std::lock_guard<std::mutex> guard(globalLoggingContext->defaultLoggerMutex);
+	return globalLoggingContext->defaultLogger.get();
+}
+
+
+} // namespace detail
+
+} // namespace log
 
 } // namespace lyra
