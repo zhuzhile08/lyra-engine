d095f287fae7391f2ff787e0fd0a5c20090f0251
Zhile <zhuzhile08@gmail.com>
2023-08-10 21:24:30 +0200

    Remove newline characters at end

--- Tests/Containers/src/main.cpp
+++ Tests/Containers/src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
 	lyra::init_filesystem(argv);
 	
 	lyra::Function<int(int, int)> addFunction(add);
-	lyra::log::debug("Result of an addition function stored in a function pointer (w. args): {}\n\n");
+	lyra::log::debug("Result of an addition function stored in a function pointer (w. args): {}\n");
 
 	lyra::Dynarray<int, 16> foo;
 	foo.resize(10);
@@ -29,37 +29,37 @@ int main(int argc, char* argv[]) {
 
 	for (const auto& it : foo) lyra::log::log("{}{} ", lyra::ansi::set_style(lyra::ansi::Font::none, 242), it);
 	lyra::log::new_line();
-	lyra::log::debug("After insert: \n");
+	lyra::log::debug("After insert:");
 	foo.insert(foo.begin() + 5, 5);
 
 	for (const auto& it : foo) lyra::log::log("{}{} ", lyra::ansi::set_style(lyra::ansi::Font::none, 242), it);
 	lyra::log::new_line();
-	lyra::log::debug("After inserting multiple elements: \n");
+	lyra::log::debug("After inserting multiple elements:");
 	foo.insert(foo.begin() + 6, 2, 6);
 
 	for (const auto& it : foo) lyra::log::log("{}{} ", lyra::ansi::set_style(lyra::ansi::Font::none, 242), it);
 	lyra::log::new_line();
-	lyra::log::debug("After erasing one element: \n");
+	lyra::log::debug("After erasing one element:");
 	foo.erase(foo.begin() + 9);
 
 	for (const auto& it : foo) lyra::log::log("{}{} ", lyra::ansi::set_style(lyra::ansi::Font::none, 242), it);
 	lyra::log::new_line();
-	lyra::log::debug("After erasing multiple elements: \n");
+	lyra::log::debug("After erasing multiple elements:");
 	foo.erase(foo.begin() + 6, foo.end() - 1);
 
 	for (const auto& it : foo) lyra::log::log("{}{} ", lyra::ansi::set_style(lyra::ansi::Font::none, 242), it);
 	lyra::log::new_line();
-	lyra::log::debug("After inserting multiple elements and causing an exception: \n");
+	lyra::log::debug("After inserting multiple elements and causing an exception:");
 	try {
 		foo.insert(foo.begin(), 30, 8);
 	} catch(...) {
-		lyra::log::exception("Catched an exception!\n\n");
+		lyra::log::exception("Catched an exception!\n");
 	}
 
 	lyra::CharVectorStream file("test.json");
 	lyra::Json jsonFile1 = lyra::Json::parse(file.data());
 
-	lyra::log::info("Json Parsing Test: {}\n\n", jsonFile1.stringify());
+	lyra::log::info("Json Parsing Test: {}\n", jsonFile1.stringify());
 
 	return 0;
 }
