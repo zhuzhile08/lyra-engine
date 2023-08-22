2d1e6496aa35c5dad7e548bf8a48112f14206d3a
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-22 12:28:24 +0200

    Add type safe enum operators

--- LyraEngine/include/Common/Utility.h
+++ LyraEngine/include/Common/Utility.h
@@ -22,13 +22,63 @@ template <typename Ty> constexpr const void* getAddress(const Ty& type) {
 	return static_cast<const void*>(type);
 }
 
-template <class Ty> concept EnumHashType = std::is_enum_v<Ty>;
+template <class Ty> concept EnumType = std::is_enum_v<Ty>;
 
 class EnumHash {
 public:
-	template <EnumHashType Ty> size_t operator()(Ty t) const {
+	template <EnumType Ty> size_t operator()(Ty t) const {
 		return static_cast<size_t>(t);
 	}
 };
+
+// credits to https://gist.github.com/StrikerX3/46b9058d6c61387b3f361ef9d7e00cd4 for these operators!
+
+template<EnumType Enum> constexpr inline Enum operator|(Enum first, Enum second) {
+	return static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) |
+		static_cast<std::underlying_type_t<Enum>>(second)
+	);
+}
+
+template<EnumType Enum> Enum constexpr inline operator&(Enum first, Enum second) {
+	return static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) &
+		static_cast<std::underlying_type_t<Enum>>(second)
+	);
+}
+
+template<EnumType Enum> Enum constexpr inline operator^(Enum first, Enum second) {
+	return static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) ^
+		static_cast<std::underlying_type_t<Enum>>(second)
+	);
+}
+
+template<EnumType Enum> Enum constexpr inline operator~(Enum first) {
+	return static_cast<Enum>(
+		~static_cast<std::underlying_type_t<Enum>>(first)
+	);
+}
+
+template<EnumType Enum> Enum constexpr inline operator|=(Enum& first, Enum second) {
+	return (first = static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) |
+		static_cast<std::underlying_type_t<Enum>>(second)
+	));
+}
+
+template<EnumType Enum> Enum constexpr inline operator&=(Enum& first, Enum second) {
+	return (first = static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) &
+		static_cast<std::underlying_type_t<Enum>>(second)
+	));
+}
+
+template<EnumType Enum> Enum constexpr inline operator^=(Enum& first, Enum second) {
+	return (first = static_cast<Enum>(
+		static_cast<std::underlying_type_t<Enum>>(first) ^
+		static_cast<std::underlying_type_t<Enum>>(second)
+	));
+}
 
 } // namespace lyra
