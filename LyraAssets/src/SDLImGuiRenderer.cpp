803f677954ad9482a1d74f7e5348c80fc9c622f8
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-27 19:34:41 +0200

    Function renames

--- LyraAssets/src/SDLImGuiRenderer.cpp
+++ LyraAssets/src/SDLImGuiRenderer.cpp
@@ -1,29 +1,29 @@
 #include "SDLImGuiRenderer.h"
 
 #include <backends/imgui_impl_sdl2.h>
-#include <backends/imgui_impl_sdlrenderer.h>
+#include <backends/imgui_impl_sdlrenderer2.h>
 
 SDLImGuiRenderer::SDLImGuiRenderer(const lyra::Window& window) : 
 	ImGuiRenderer(window), 
 	m_renderer(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED)) {
 	ImGui_ImplSDL2_InitForSDLRenderer(window.get(), m_renderer);
-    ImGui_ImplSDLRenderer_Init(m_renderer);
+    ImGui_ImplSDLRenderer2_Init(m_renderer);
 }
 
 SDLImGuiRenderer::~SDLImGuiRenderer() {
-	ImGui_ImplSDLRenderer_Shutdown();
+	ImGui_ImplSDLRenderer2_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
 	SDL_DestroyRenderer(m_renderer);
 }
 
 void SDLImGuiRenderer::beginFrame() {
-	ImGui_ImplSDLRenderer_NewFrame();
+	ImGui_ImplSDLRenderer2_NewFrame();
 	ImGui_ImplSDL2_NewFrame();
 }
 
 void SDLImGuiRenderer::endFrame() {
 	SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 0);
 	SDL_RenderClear(m_renderer);
-	ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+	ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
 	SDL_RenderPresent(m_renderer);
 }
