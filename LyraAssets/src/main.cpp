6390e2639940b1de60b879f7c71d980f2637a63b
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-27 17:47:33 +0200

    Draw window

--- LyraAssets/src/main.cpp
+++ LyraAssets/src/main.cpp
@@ -1,5 +1,10 @@
 #define VERSION "1.00"
 
+#include "ContentManager.h"
+#include "SDLImGuiRenderer.h"
+#include "GuiElements.h"
+#include "IconsCodicons.h"
+
 #include <Lyra/Lyra.h>
 
 #include <Common/Logger.h>
@@ -10,13 +15,34 @@
 #include <Input/Input.h>
 
 #include <imgui.h>
+#include <imgui_internal.h>
 
 int main(int argc, char* argv[]) {
 	lyra::init();
-	ImGui::CreateContext();
-	
-	lyra::Window window;
-	
 	lyra::initFileSystem(argv);
-	lyra::initInputSystem(window, ImGui::GetCurrentContext());	
+
+	lyra::Window window("LyraAssets - Lyra Engine Assets Pipeline Tool", lyra::Window::Flags::resizable, {860, 645});
+	SDLImGuiRenderer guiRenderer(window);
+	
+	lyra::initInputSystem(window, ImGui::GetCurrentContext());
+
+	ContentManager contentManager;
+	
+	ImFontConfig config;
+	config.MergeMode = true; 
+	config.PixelSnapH = true; 
+	config.GlyphMinAdvanceX = 15.0f;
+	config.GlyphOffset = {0, 4.5f};
+	ImWchar range[] =  { ICON_MIN_CI, ICON_MAX_16_CI, 0 };
+	guiRenderer.setIconFont("data/fonts/codicon.ttf", config, range, 15.0f);
+
+	gui::ButtonBar buttonBar(guiRenderer);
+	gui::MainMenuBar mainMenuBar(window, guiRenderer);
+
+	while (window.running()) {
+		lyra::input::update();
+		guiRenderer.draw();
+	}
+
+	lyra::quit();
 }
