9616711258fa8f22c61589d498fac376b974e553
Zhile Zhu <zhuzhile08@gmail.com>
2023-08-28 20:37:47 +0200

    Add file for content manager implementation

---
+++ LyraAssets/src/ContentManager.h
@@ -0,0 +1,26 @@
+/*************************
+ * @file ContentManagerGuiRenderer.h
+ * @author Zhile Zhu (zhuzhile08@gmail.com)
+ * 
+ * @brief The content manager
+ * @brief Handles rendering and its file system
+ * 
+ * @date 2023-08-23
+ * @copyright Copyright (c) 2022
+ *************************/
+
+#pragma once
+
+#include "IconsCodicons.h"
+
+#include <Lyra/Lyra.h>
+
+#include <Common/FileSystem.h>
+
+#include <Graphics/SDLWindow.h>
+
+#include <SDL_render.h>
+
+struct ContentManager {
+	
+};
