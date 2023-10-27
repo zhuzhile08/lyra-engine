1f44253972f1e9095e24e3c34ca0c19480faa594
Zhile <zhuzhile08@gmail.com>
2023-10-27 21:12:03 +0200

    Start reimplementing the camera

--- LyraEngine/include/EntitySystem/Camera.h
+++ LyraEngine/include/EntitySystem/Camera.h
@@ -11,123 +11,65 @@
 
 #pragma once
 
-#include <Common/Common.h>
-
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
-#include <vector>
+#include <Common/Common.h>
 #include <Common/Array.h>
-
-#include <glm/glm.hpp>
-#include <Math/ComputePipeline.h>
-
 #include <Common/UniquePointer.h>
-#include <Common/ResourcePool.h>
+#include <Common/RenderSystem.h>
 #include <Common/Config.h>
 
 #include <EntitySystem/Entity.h>
 
-#include <Graphics/VulkanImpl/DescriptorSystem.h>
-#include <Graphics/VulkanImpl/GPUBuffer.h>
-#include <Graphics/GraphicsPipelineSystem.h>
-#include <Graphics/Renderer.h>
+#include <Graphics/VulkanRenderSystem.h>
+
+#include <glm/glm.hpp>
+
+#include <vector>
 
 namespace lyra {
 
-/**
- * @brief wrapper arond a uniform buffer object
- */
-class Camera : public Framebuffers, public ComponentBase {
+class Camera : public ComponentBase, public QueuedRenderSystem, public RenderObject {
 public:
-
-	// projection mode of the camera
 	enum Projection {
 		PROJECTION_PERSPECTIVE,
 		PROJECTION_ORTHOGRAPHIC
 	};
 
-	// projection mode of the camera
 	enum Clear {
 		CLEAR_SKYBOX,
 		CLEAR_COLOR,
 		CLEAR_NONE
 	};
 
-	// camera data
 	struct CameraData {
 		alignas(16) const glm::mat4 model;
 		alignas(16) const glm::mat4 proj;
 	};
-	
-	/**
-	 * @brief construct a camera node
-	 *
-	 * @param skybox skybox for the camera
-	 * @param perspective check if perspective is true or not
-	 */
 	Camera(
-		Skybox* skybox = nullptr,
+		// Skybox* skybox = nullptr,
 		bool perspective = true
 	);
 	DEFINE_DEFAULT_MOVE(Camera)
 
-	/**
-	 * @brief recreate the camera
-	 */
 	void recreate();
 
-	/**
-	 * @brief set the projection of the camera in perspective mode
-	 * 
-	 * @param fov field of view
-	 * @param near near clipping plane
-	 * @param far far clipping plane
-	*/
 	void setPerspective(const float32& fov = 45.0f, const float32& near = 0.1f, const float32& far = 10.0f) noexcept;
-	/**
-	 * @brief set the projection of the camera in orthographic mode
-	 *
-	 * @param viewport viewport
-	 * @param near near clipping plane
-	 * @param far far clipping plane
-	*/
 	void setOrthographic(const glm::vec4& viewport = { 0.0f, 0.0f, 1.0f, 1.0f }, const float32& near = 0.1f, const float32& far = 20.0f) noexcept;
-	/**
-	 * @brief update function
-	*/
+
 	void update();
 
-	/**
-	 * @brief get the field of view of the camera
-	 *
-	 * @return float32
-	*/
 	NODISCARD float32 fov() const noexcept { return m_fov; }
-	/**
-	 * @brief get the near clipping plane of the camera
-	 *
-	 * @return float32
-	*/
 	NODISCARD float32 near() const noexcept { return m_near; }
-	/**
-	 * @brief get the far clipping plane of the camera
-	 *
-	 * @return float32
-	*/
 	NODISCARD float32 far() const noexcept { return m_far; }
-	/**
-	 * @brief get the viewport dimensions and positions
-	 *
-	 * @return glm::vec4
-	*/
 	NODISCARD glm::vec4 viewport() const noexcept { return m_viewport; }
 
 private:
 	std::vector<Material*> m_materials;
 	Array<vulkan::GPUBuffer, config::maxFramesInFlight> m_buffers;
 	Array<vulkan::DescriptorSystem::DescriptorSetResource, config::maxFramesInFlight> m_descriptorSets;
-	ComputePipeline m_computePipeline;
-	Skybox* m_skybox;
+	// ComputePipeline m_computePipeline;
+	// Skybox* m_skybox;
 
 	float32 m_fov = 45.0f, m_near = 0.1f, m_far = 20.0f, m_depth = 1.0f;
 	glm::vec4 m_viewport = { 0.0f, 0.0f, 1.0f, 1.0f };
