bbe898a5601ace17b5410af656151da78e6c019f
Zhile Zhu <zhuzhile08@gmail.com>
2023-11-22 20:34:58 +0100

    Update engine test main

--- Tests/Engine/src/main.cpp
+++ Tests/Engine/src/main.cpp
@@ -6,10 +6,12 @@
 #include <Graphics/VulkanRenderSystem.h>
 #include <Graphics/Renderer.h>
 #include <Graphics/SDLWindow.h>
+#include <Graphics/Material.h>
 #include <Graphics/Texture.h>
 #include <Graphics/Mesh.h>
 
 #include <EntitySystem/Camera.h>
+#include <EntitySystem/MeshRenderer.h>
 
 #include <Resource/ResourceSystem.h>
 
@@ -24,7 +26,14 @@ int main(int argc, char* argv[]) {
 	lyra::Window window;
 
 	lyra::initInputSystem(window);
-	lyra::initRenderSystem({{0, 7, 0}, &window});
+	lyra::initRenderSystem({0, 7, 0}, window);
+
+	lyra::Entity sceneRoot;
+
+	lyra::Entity camera (sceneRoot, "Camera");
+	camera.addComponent<lyra::Camera>();
+
+	/**
 
 	auto& vertexShader = lyra::resource::shader("shader/vert.spv");
 	auto& fragmentShader = lyra::resource::shader("shader/frag.spv");
@@ -37,17 +46,32 @@ int main(int argc, char* argv[]) {
 
 	lyra::renderSystem::Renderer renderer(framebuffers, graphicsProgram);
 
+	lyra::Material roomMat(
+		lyra::Color(),
+		{ &texture }
+	);
+
 	lyra::Camera camera;
 
 	renderer.bindCamera(camera);
 
+	lyra::MeshRenderer roomRenderer(room, roomMat);
+
+	renderer.m_meshes.emplace(&roomMat, std::vector<lyra::MeshRenderer*>{ &roomRenderer });
+
+	*/
+
+	auto p = lyra::renderer::graphicsPipeline(lyra::resource::shader("shader/vert.spv"), lyra::resource::shader("shader/frag.spv"));
+
+	lyra::renderer::setScene(sceneRoot);
+
 	while (window.running()) {
 		lyra::input::update();
-		if (!lyra::renderSystem::beginFrame()) continue;
+		if (!lyra::renderer::beginFrame()) continue;
 
-		lyra::renderSystem::draw();
+		lyra::renderer::draw();
 
-		lyra::renderSystem::endFrame();
+		lyra::renderer::endFrame();
 	}
 
 	lyra::quit();
