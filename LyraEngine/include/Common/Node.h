c73c2c2d5364aad510a233726438cb098ee285cb
Zhile Zhu <zhuzhile08@gmail.com>
2024-02-22 19:16:46 +0100

    Better node implementation

--- LyraEngine/include/Common/Node.h
+++ LyraEngine/include/Common/Node.h
@@ -12,10 +12,11 @@
 #pragma once
 
 #include <Common/Common.h>
+#include <Common/UniquePointer.h>
+#include <Common/SharedPointer.h>
 
 #include <type_traits>
 #include <utility>
-#include <functional>
 
 #include <string>
 #include <string_view>
@@ -23,48 +24,60 @@
 
 namespace lyra {
 
-// Multi linked list implementation in the style of standard library containers
-// Creation inspired by Godot's Node system, therefore also called node
-// Can be used as a normal class member, but more recommended to use it like this to provide a better interface:
-// class Foo : Node<Foo> {}
-// Therefore also the pointers to "self"
-template <class Ty, class Key = std::string, class HashOrCompare = std::hash<Key>, template<class...> class Container = std::unordered_map> class Node {
+template <
+	class Type, 
+	template<class...> class SmartPointer = UniquePointer,
+	class Key = std::string, 
+	class HashOrCompare = std::hash<Key>, 
+	template<class...> class Container = std::unordered_map> 
+class BasicNode {
 public:
-	using value_type = Ty;
+	using value_type = Type;
 	using const_value = const value_type;
 	using reference = value_type&;
 	using const_reference = const value_type&;
+	using movable = value_type&&;
 	using pointer = value_type*;
 	using const_pointer = const pointer;
-	using movable = value_type&&;
+	using pointer_const = const_value*;
+	using smart_pointer = SmartPointer<value_type>;
+
 	using key_type = Key;
-	using hash_function = HashOrCompare;
-	using container = Container<key_type, pointer, hash_function>;
 	using key_reference = key_type&;
 	using const_key_reference = const key_type&;
 	using key_rvreference = key_type&&;
+
+	using hash_function = HashOrCompare;
+	using container = Container<key_type, smart_pointer, hash_function>;
+
 	using iterator = typename container::iterator;
 	using const_iterator = typename container::const_iterator;
 	using iterator_pair = std::pair<iterator, bool>;
 
-	Node() = default;
-	template <class NameType> Node(
-		pointer self, 
-		NameType name,
+	BasicNode() = default;
+	template <class NameType, class... Args> BasicNode(
+		NameType&& name,
 		reference parent
-	) noexcept : m_name(name), m_self(self) { parent.insert_child(self); }
-	template <class NameType> Node(
-		pointer self,
-		NameType name,
+	) noexcept : m_name(std::forward<NameType>(name)) { parent.insert(dynamic_cast<pointer>(this)); }
+	template <class NameType, class... Args> BasicNode(
+		NameType&& name,
 		movable parent
-	) noexcept : m_name(name), m_self(self) { parent.insert_child(self); }
-	template <class NameType> Node(
-		pointer self,
-		NameType name,
+	) noexcept : m_name(std::forward<NameType>(name)) { parent.insert(dynamic_cast<pointer>(this)); }
+	template <class NameType, class... Args> BasicNode(
+		NameType&& name,
 		pointer parent = nullptr
-	) noexcept : m_name(name), m_self(self) { if (parent) parent->insert_child(self); }
+	) noexcept : m_name(std::forward<NameType>(name)) { if (parent) parent->insert(dynamic_cast<pointer>(this)); }
+	BasicNode(BasicNode&&) = default;
+	BasicNode(const BasicNode&) requires std::is_copy_assignable_v<smart_pointer> = default;
+	virtual ~BasicNode() = default;
 
-	void clear() noexcept { m_children.clear(); }
+	BasicNode& operator=(BasicNode&&) = default;
+	BasicNode& operator=(const BasicNode&) requires std::is_copy_assignable_v<smart_pointer> = default;
+
+	reference clear() noexcept { 
+		m_children.clear();
+		return *this;
+	}
 
 	iterator begin() noexcept {
 		return m_children.begin();
@@ -86,55 +99,41 @@ public:
 		return m_children.end();
 	}
 
-	iterator_pair insert_child(reference child) {
-		child.m_parent = m_self;
-		return m_children.insert({child.m_name, &child});
+	reference insert(movable child) {
+		child.m_parent = this;
+		return *m_children.emplace(child.m_name, smart_pointer::create(std::move(child))).first->second.get();
 	}
-	iterator_pair insert_child(movable child) {
-		child.m_parent = m_self;
-		return m_children.insert({child.m_name, &child});
+	reference insert(pointer child) {
+		child->m_parent = this;
+		return *m_children.emplace(child->m_name, smart_pointer(child)).first->second.get();
 	}
-	iterator_pair insert_child(pointer child) {
-		child->m_parent = m_self;
-		return m_children.insert({child->m_name, child});
-	}
-
-	void insert_behind(reference parent) { 
-		parent.insert_child(m_self);
-	}
-	void insert_behind(movable parent) { 
-		parent.insert_child(m_self);
-	}
-	void insert_behind(pointer parent) { 
-		if (parent) parent->insert_child(m_self);
-	}
-	void rename(const_key_reference name) {
-		m_parent->erase(m_name);
-		m_name = name;
-		insert_behind(m_parent);
-	}
-	void rename(key_rvreference name) {
-		m_parent->erase(m_name);
-		m_name = std::move(name);
-		insert_behind(m_parent);
-	}
-	template <class KeyType> void rename(KeyType name) {
-		m_parent->erase(m_name);
-		m_name = std::move(name);
-		insert_behind(m_parent);
+	template <class... Args> reference insert(Args&&... args) {
+		auto child = smart_pointer::create(std::forward<Args>(args)...);
+		child->m_parent = this; // for extra safety
+		return *m_children.emplace(child->m_name, std::move(child)).first->second.get();
 	}
 
-	iterator erase(iterator pos) { 
-		return m_children.erase(pos); 
+	template <class KeyType> reference rename(KeyType&& name) {
+		auto t = dynamic_cast<pointer>(this);
+		m_parent->m_children.extract(std::exchange(m_name, std::forward<KeyType>(name)));
+		m_parent->m_children.emplace(m_name, smart_pointer(t));
+		return *t;
 	}
-	iterator erase(const_iterator pos) { 
+
+	reference erase(iterator pos) { 
 		return m_children.erase(pos); 
+		return *dynamic_cast<pointer>(this);
 	}
-	iterator erase(const_iterator first, const_iterator last) { 
+	reference erase(const_iterator pos) { 
+		return m_children.erase(pos); 
+		return *dynamic_cast<pointer>(this);
+	}
+	reference erase(const_iterator first, const_iterator last) { 
 		return m_children.erase(first, last); 
+		return *dynamic_cast<pointer>(this);
 	}
-	size_t erase(const_key_reference name) { 
-		return m_children.erase(name); 
+	template <class KeyType> requires std::is_convertible_v<KeyType, key_type> size_t erase(KeyType&& name)  { 
+		return m_children.erase(std::forward(name)); 
 	}
 
 	void swap(reference other) noexcept { 
@@ -147,48 +146,84 @@ public:
 	NODISCARD bool empty() const noexcept { 
 		return m_children.empty(); 
 	}
-	NODISCARD bool no_children() const noexcept { 
-		return m_children.empty(); 
-	}
 	operator bool() const noexcept { 
 		return m_children.empty(); 
 	}
 
-	iterator find(key_reference name) { 
-		return m_children.find(name); 
+	template <class KeyType> iterator find(KeyType&& name) { 
+		return m_children.find(std::forward<KeyType>(name)); 
 	}
-	const_iterator find(key_reference name) const { 
-		return m_children.find(name); 
+	template <class KeyType> const_iterator find(KeyType&& name) const { 
+		return m_children.find(std::forward<KeyType>(name)); 
 	}
-	bool contains(key_reference name) const { 
-		return m_children.contains(name); 
+	template <class KeyType> bool contains(KeyType&& name) const { 
+		return m_children.contains(std::forward<KeyType>(name)); 
 	}
 
-	const_reference at(const_key_reference name) const { return *m_children.at(name); }
-	template <class KeyType> const_reference at(KeyType name) const { return *m_children.at(name); }
-	reference at(const_key_reference name) { return *m_children.at(name); }
-	template <class KeyType> reference at(KeyType name) { return *m_children.at(name); }
+	template <class KeyType> const_reference child(KeyType&& key) const {
+		constexpr bool stringlike = requires(const KeyType& k) {
+			key_type(key);
+			key_type().find("");
+			key_type().substr(0, 0);
+		};
 
-	const_reference operator/(const_key_reference name) const { return *m_children.at(name); }
-	template <class KeyType> const_reference operator/(KeyType name) const { return *m_children.at(name); }
-	reference operator/(const_key_reference name) { return *m_children.at(name); }
-	template <class KeyType> reference operator/(KeyType name) { return *m_children.at(name); }
+		if constexpr (stringlike) {
+			key_type k(key);
+			size_t beg = 0, cur;
+			pointer_const p = dynamic_cast<pointer_const>(this);
+
+			while ((cur = k.find("::", beg)) < k.size()) {
+				p = p->m_children.at(k.substr(beg, cur - beg));
+				(beg = cur) += 2;
+			}
+
+			return *p->m_children.at(k.substr(beg)).get();
+		} else {
+			return *m_children.at(key).get();
+		}
+	}
+	template <class KeyType> reference child(KeyType&& key) {
+		constexpr bool stringlike = requires(const KeyType& k) {
+			key_type(key);
+			key_type().find("");
+			key_type().substr(0, 0);
+		};
+
+		if constexpr (stringlike) {
+			key_type k(key);
+			size_t beg = 0, cur;
+			pointer p = dynamic_cast<pointer>(this);
+
+			while ((cur = k.find("::", beg)) < k.size()) {
+				p = p->m_children.at(k.substr(beg, cur - beg));
+				beg = cur + 2;
+			}
+
+			return *p->m_children.at(k.substr(beg)).get();
+		} else {
+			return *m_children.at(key).get();
+		}
+	}
+
+	template <class KeyType> const_reference at(KeyType&& name) const { *m_children.at(std::forward<KeyType>(name)); }
+	template <class KeyType> reference at(KeyType&& name) { *m_children.at(std::forward<KeyType>(name)); }
 	
-	reference operator[](const_key_reference name) { return *m_children[name]; }
-	template <class KeyType> const_reference operator[](KeyType name) const { return *m_children[name]; }
-	reference operator[](key_rvreference name) { return *m_children[name]; }
-	template <class KeyType> reference operator[](KeyType name) { return *m_children[name]; }
+	template <class KeyType> const_reference operator[](KeyType&& name) const { return *m_children[std::forward<KeyType>(name)]; }
+	template <class KeyType> reference operator[](KeyType&& name) { return *m_children[std::forward<KeyType>(name)]; }
 
 	NODISCARD size_t size() const noexcept { return m_children.size(); }
-	NODISCARD std::string name() const noexcept { return m_name; }
-	NODISCARD const_pointer const parent() const noexcept { return m_parent; }
+	NODISCARD key_type name() const noexcept { return m_name; }
+	NODISCARD pointer parent() noexcept { return dynamic_cast<pointer>(m_parent); }
+	NODISCARD const_pointer const parent() const noexcept { return dynamic_cast<pointer>(m_parent); }
 
 protected:
-	Key m_name;
+	key_type m_name;
 
-	pointer m_self;
-	pointer m_parent = nullptr;
+	BasicNode* m_parent = nullptr;
 	container m_children;
 };
+
+template <class Ty> using Node = BasicNode<Ty, UniquePointer>;
+template <class Ty> using SharedNode = BasicNode<Ty, SharedPointer>;
 
 } // namespace lyra
