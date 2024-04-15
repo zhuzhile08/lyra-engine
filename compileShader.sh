a5ec282094ef61feac928c28c2ff9eb04d838480
Zhile Zhu <zhuzhile08@gmail.com>
2024-04-15 15:22:32 +0200

    Compile shaders also to LyraAssets

--- compileShader.sh
+++ compileShader.sh
@@ -1,5 +1,8 @@
 /usr/local/bin/glslc LyraEngine/src/shaders/src/vert.vert -o data/shader/vert.spv
 /usr/local/bin/glslc LyraEngine/src/shaders/src/frag.frag -o data/shader/frag.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/vert.vert -o LyraAssets/data/shader/vert.spv
+/usr/local/bin/glslc LyraEngine/src/shaders/src/frag.frag -o LyraAssets/data/shader/frag.spv
+
 /usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.vert -o data/shader/cubemapVert.spv
 /usr/local/bin/glslc LyraEngine/src/shaders/src/cubemap.frag -o data/shader/cubemapFrag.spv
 /usr/local/bin/glslc LyraEngine/src/shaders/src/skybox.vert -o data/shader/skyboxVert.spv
